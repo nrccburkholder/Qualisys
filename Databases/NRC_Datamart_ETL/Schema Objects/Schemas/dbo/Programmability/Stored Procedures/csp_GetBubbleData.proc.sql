171476eaea9070a329a2821674634d27957aace3
ccaouette <ccaouette@wsccaouette.nationalresearch.com>
2014-05-28 12:13:33 -0500

    CHG0031279-INC0033028 Resolve ETL/DataMove issue

--- Databases/NRC_Datamart_ETL/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/csp_GetBubbleData.proc.sql
+++ Databases/NRC_Datamart_ETL/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/csp_GetBubbleData.proc.sql
@@ -2,15 +2,26 @@
 	@ExtractFileID int
 AS
 	SET NOCOUNT ON 	
- 	
---	declare @ExtractFileID int
---	set @ExtractFileID= 20 -- test only
 
-    select ExtractFileID,LithoCode,nrcQuestionCore,responseVal,QUESTIONFORM_ID--*
-     from dbo.BubbleTemp with (NOLOCK)     
-     WHERE ExtractFileID = @ExtractFileID               
-     
+	 --	declare @ExtractFileID int
+	--	set @ExtractFileID= 20 -- test only
+
+    declare @rbCount int
+	set @rbCount = 0
+
+	Select @rbCount = COUNT(*) from dbo.BubbleTemp  WHERE ExtractFileID = @ExtractFileID 
+
+	If @rbCount > 0
+	Begin
+	    select ExtractFileID,LithoCode,nrcQuestionCore,responseVal,QUESTIONFORM_ID--*
+		from dbo.BubbleTemp with (NOLOCK)     
+		WHERE ExtractFileID = @ExtractFileID       
+	End
+	Else
+	Begin
+		select @ExtractFileID,0,0,0,0
+	End
+        
 
 --exec csp_GetBackgroundData 20
-
-
+GO
