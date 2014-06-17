26ed82cca00f7a74bb27c7098e6dc721a21a70de
tbutler <tbutler@nationalresearch.com>
2014-06-17 12:12:27 -0500

    Config Mgr - add SurveySubType class

---
+++ QualiSys Class Library Solution/QualiSys.Library/SurveySubType.vb
@@ -0,0 +1,52 @@
+﻿Public Class SurveySubType
+
+#Region "private fields"
+    Private mSurveySubType_Id As Integer
+    Private mDescription As String = String.Empty
+#End Region
+
+#Region "public properties"
+
+    Public Property Id() As Integer
+        Get
+            Return mSurveySubType_Id
+        End Get
+        Private Set(ByVal value As Integer)
+            If Not value = mSurveySubType_Id Then
+                mSurveySubType_Id = value
+            End If
+        End Set
+    End Property
+
+    Public Property Description() As String
+        Get
+            Return mDescription
+        End Get
+        Set(ByVal value As String)
+            If value Is Nothing Then value = String.Empty
+            If Not value = mDescription Then
+                mDescription = value
+            End If
+        End Set
+    End Property
+
+#End Region
+
+#Region " Constructors "
+
+    Private Sub New()
+
+
+    End Sub
+
+
+    Private Sub New(ByVal Id As Integer, ByVal Desc As String)
+
+        mSurveySubType_Id = Id
+        mDescription = Desc
+
+    End Sub
+
+#End Region
+
+End Class
