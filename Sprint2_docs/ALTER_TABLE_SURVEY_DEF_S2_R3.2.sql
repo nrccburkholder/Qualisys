6c6a6e7b12e0bbab30ea9ca97142b2fb169288f1
tbutler <tbutler@nationalresearch.com>
2014-06-24 10:40:49 -0500

    Squashed commit of the following:
    commit ecd45b051bc80335e46c6973f064c251341acc73
    Author: tbutler <tbutler@nationalresearch.com>
    Date:   Tue Jun 24 10:38:02 2014 -0500
    
        New: Add Sprint2 scripts

--- Sprint2_docs/ALTER_TABLE_SURVEY_DEF_S2_R3.2.sql
+++ Sprint2_docs/ALTER_TABLE_SURVEY_DEF_S2_R3.2.sql
@@ -1,6 +1,6 @@
 USE QP_PROD
 
 
--- add QuestionnaireType_ID to SURVEY_DEF
+-- add QuestionaireType_ID to SURVEY_DEF
 ALTER TABLE SURVEY_DEF
-ADD QuestionnaireType_ID int NULL
+ADD QuestionaireType_ID int NULL
