8c9ef21114e6cb949a39b19b8f8ad08247dc4206
Chris Burkholder <cburkholder@nationalresearch.com>
2014-07-08 14:16:06 -0500

    Fixed Catalyst SampleUnit CAHPS insert S3 US4
    Based on Corry's advice, I added an insert of client records and survey_def records to the script here in order to actually get the SampleUnit records over to Catalyst.  I also added logic to only move surveytype of 4,8,10 (CG CAHPS, ICH CAHPS, ACO CAHPS) since HCAHPS and HHCAHPS were already getting moved over according to the old Catalyst ETL SP.

--- Sprint3_docs/NRC_DataMart_ETL.Insert dbo.ExtractQueue S3 US4.sql
+++ Sprint3_docs/NRC_DataMart_ETL.Insert dbo.ExtractQueue S3 US4.sql
@@ -3,9 +3,18 @@ use [QP_Prod]
 
 --select top 10 * from sampleunit where cahpstype_id <> 0
 
+--EXEC sp_helptext 'dbo.trg_NRC_DataMart_ETL_dbo_CLIENT'
+	insert NRC_DataMart_ETL.dbo.ExtractQueue (EntityTypeID, PKey1, PKey2, IsMetaData,Source)
+		select 1, CLIENT_ID, NULL, 1 ,'Sprint3 Task 5.X Backfill IsCAHPS CJB 6/30/2014' from client
+		where client_id in (select client_id from survey_def where survey_ID in (select survey_id from sampleunit where CAHPSType_id in (4,8,10)))
 
---EXEC sp_helptext 'dbo.trg_NRC_DataMart_ETL_dbo_SAMPLEUNIT'
+--EXEC sp_helptext 'dbo.trg_NRC_DataMart_ETL_Ins_dbo_SURVEY_DEF'
+	insert NRC_DataMart_ETL.dbo.ExtractQueue (EntityTypeID, PKey1, PKey2, IsMetaData,Source)
+		select 3, SURVEY_ID, NULL, 1 ,'Sprint3 Task 5.X Backfill IsCAHPS CJB 6/30/2014'
+		from survey_def where survey_ID in (select survey_id from sampleunit where CAHPSType_id in (4,8,10))
+
+--EXEC sp_helptext 'dbo.trg_NRC_DataMart'
 
  insert NRC_DataMart_ETL.dbo.ExtractQueue (EntityTypeID, PKey1, PKey2, IsMetaData,Source)  
   select 6, SAMPLEUNIT_ID, NULL, 1,'Sprint3 Task 5.X Backfill IsCAHPS CJB 6/30/2014'   from
-  sampleunit where CAHPSType_id <> 0
+  sampleunit where CAHPSType_id in (4,8,10)
