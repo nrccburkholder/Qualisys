d274ed5ec55e537f99c499bca92484b3b4d43dfe
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2014-07-16 12:46:36 -0500

    changes to the [PCLGEN - sp_pcl_batch_populatepos] job to address missing comment box data

--- Databases/QP_Prod/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/sp_pcl_batch_populatepos.proc.sql
+++ Databases/QP_Prod/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/sp_pcl_batch_populatepos.proc.sql
@@ -6,13 +6,14 @@ AS
  declare @sqlstr varchar(255)
  select @startdtime = getdate()
  while exists (select batch_id
-   from dbo.PCLQuestionForm
-   where  bitIsProcessed = 0
-   and batch_id not in (select batch_id
-      from dbo.PCLGenPosError
-      where isResolved = 0)
- ) 
+			   from qp_scan.dbo.PCLQuestionForm
+			   where bitIsProcessed = 0
+			   and Questionform_id not in (	select Questionform_id
+											from qp_prod.dbo.PCLGenPosError
+											where isResolved = 0 )
+				) 
  begin
+/*
   select @starttime = numParam_value
   from dbo.qualpro_params
   where strParam_nm = 'PCLGenPosStartTime' and strParam_grp = 'PCLGen'
@@ -26,7 +27,9 @@ AS
    @stoptime = isnull(@stoptime,0),
    @pause = isnull(@pause,0),
    @nowtime = (datepart(hh,getdate()) * 100) + datepart(mi,getdate())
+*/
 /* If the batch windows is closed, then we will raise an error for it to retry later. */
+/*
   if ((@pause > 0) OR
      (@nowtime > @stoptime and @nowtime < @starttime and @starttime > @stoptime) OR
      (@nowtime > @stoptime and @starttime < @stoptime) OR
@@ -35,32 +38,31 @@ AS
    raiserror ('Batch window is closed, enter retry looping.', 16, 1)
    return
   end
+*/
   select @batch_id = min(batch_id)
-  from dbo.pclquestionform
+  from qp_scan.dbo.pclquestionform
   where bitIsProcessed = 0
-  and batch_id not in (select batch_id
-     from dbo.PCLGenPosError
-     where isResolved = 0)
-  exec @rc=dbo.sp_pcl_populate_pos_tables @batch_id
+  and questionform_id not in (	select questionform_id
+								 from qp_prod.dbo.PCLGenPosError
+								 where isResolved = 0)	 
+  exec dbo.sp_pcl_populate_pos_tables @batch_id
   select @err = @@error
   if @err <> 0
   begin
-   INSERT INTO dbo.PCLGenPosError (batch_id, sql_error, returned_error)
-   VALUES (@batch_id, @err, @rc)
+	INSERT INTO qp_scan.dbo.PCLGenPosError (batch_id, sql_error, returned_error)
+	VALUES (@batch_id, @err, @rc)
   end
   else if @rc <> 0
   begin
-   INSERT INTO dbo.PCLGenPosError (batch_id, sql_error, returned_error)
-   VALUES (@batch_id, @err, @rc)
+	INSERT INTO qp_scan.dbo.PCLGenPosError (batch_id, sql_error, returned_error)
+	VALUES (@batch_id, @err, @rc)
   end
  end
  select @enddtime = getdate()
  if exists (select batch_id
-   from dbo.PCLGenPosError
-   where datgenerated between @startdtime and @enddtime)
+			from qp_scan.dbo.PCLGenPosError
+			where datgenerated between @startdtime and @enddtime)
  begin
   select @sqlstr = 'SELECT * FROM dbo.PCLGenPosError WHERE datgenerated BETWEEN ''' + convert(varchar,@startdtime,101) + ''' AND ''' + convert(varchar,@enddtime,101) + ''''
   exec dbo.sp_RaiseError 2, 'PCLGenPos', @sqlstr
  end
-
-
