7cc20804964402d103f81aaaa5e35c828fc16de6
tbutler <tbutler@nationalresearch.com>
2014-08-22 14:40:51 -0500

    NEW: USPS_ACS Service

---
+++ USPS_ACS Solution/USPS_ACS_Service/USPS_ACS_Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USPS_ACS_Service
+{
+    public partial class USPS_ACS_Service : ServiceBase
+    {
+        public USPS_ACS_Service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
