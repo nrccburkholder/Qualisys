d8e61dc5c7553ea86e56454dec22b6b91ddaea2a
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2014-09-19 15:02:06 -0500

    S9 US6 T1 update USPS_ACS_SelectPartialMatches

--- Sprint_docs/Sprint9_docs/S9 US6 T1 ALTER_PROC_USPS_ACS_ProcessFile.sql
+++ Sprint_docs/Sprint9_docs/S9 US6 T1 ALTER_PROC_USPS_ACS_ProcessFile.sql
@@ -7,6 +7,7 @@ Dave Gilsdorf
 
 ALTER TABLE dbo.USPS_ACS_ExtractFile_PartialMatch
 ALTER PROCEDURE dbo.USPS_ACS_ProcessFile
+alter procedure dbo.usps_acs_selectpartialmatches
 */
 use qp_prod
 go
@@ -198,5 +199,71 @@ where f.USPS_ACS_ExtractFileLog_ID=@USPS_ACS_ExtractFileLog_ID
 drop table #pop
 drop table #studyList
 drop table #matches
+go
+ALTER PROCEDURE [dbo].[USPS_ACS_SelectPartialMatches]
+@MarkNotified as bit = 0
+
+AS
+BEGIN
+	SELECT [USPS_ACS_ExtractFile_PartialMatch_id]
+      ,pm.[Study_id]
+      ,pm.[Pop_id]
+      ,PM.[strLithocode]
+      ,pm.[Status]
+      ,pm.[FullMatch]
+      ,pm.[popFname]
+      ,pm.[popLname]
+      ,pm.[popAddr]
+      ,pm.[popAddr2]
+      ,pm.[popCity]
+      ,pm.[popSt]
+      ,pm.[popZip5]
+      ,pm.[USPS_ACS_ExtractFile_Work_ID]
+      ,pm.[USPS_ACS_ExtractFile_ID]
+      ,pm.[FName]
+      ,pm.[LName]
+      ,pm.[PrimaryNumberOld]
+      ,pm.[PreDirectionalOld]
+      ,pm.[StreetNameOld]
+      ,pm.[StreetSuffixOld]
+      ,pm.[PostDirectionalOld]
+      ,pm.[UnitDesignatorOld]
+      ,pm.[SecondaryNumberOld]
+      ,pm.[CityOld]
+      ,pm.[StateOld]
+      ,pm.[Zip5Old]
+      ,pm.[PrimaryNumberNew]
+      ,pm.[PreDirectionalNew]
+      ,pm.[StreetNameNew]
+      ,pm.[StreetSuffixNew]
+      ,pm.[PostDirectionalNew]
+      ,pm.[UnitDesignatorNew]
+      ,pm.[SecondaryNumberNew]
+      ,pm.[CityNew]
+      ,pm.[StateNew]
+      ,pm.[Zip5New]
+      ,pm.[Plus4ZipNew]
+      ,pm.[AddressNew]
+      ,pm.[Address2New]
+	INTO #PartialMatches
+	FROM [dbo].[USPS_ACS_ExtractFile_PartialMatch] pm
+	INNER JOIN [dbo].[USPS_ACS_ExtractFile] ef on ef.usps_acs_extractfile_id = pm.usps_acs_extractfile_id
+	WHERE ef.IsNotified = 0
+
+	-- set IsNotified to 1 for each partial match returned
+	if @MarkNotified = 1
+	begin
+		Update ef
+			SET IsNotified = 1
+		FROM USPS_ACS_ExtractFile ef
+		INNER JOIN #PartialMatches pm ON pm.USPS_ACS_ExtractFile_ID = ef.USPS_ACS_ExtractFile_ID
+	end
+
+	SELECT *
+	FROM #PartialMatches
+
+	DROP TABLE #PartialMatches
+
+END
 go
 commit tran
