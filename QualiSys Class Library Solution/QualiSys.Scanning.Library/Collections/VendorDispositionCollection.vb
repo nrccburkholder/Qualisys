15b0508805ac9d44f5c988b1516fa31b7d2f5fb7
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2014-09-25 10:48:32 -0500

    S9 US13 T3 create tbl/proc CoverLtrArtifactUnitMap

--- QualiSys Class Library Solution/QualiSys.Scanning.Library/Collections/VendorDispositionCollection.vb
+++ QualiSys Class Library Solution/QualiSys.Scanning.Library/Collections/VendorDispositionCollection.vb
@@ -3,11 +3,21 @@ Imports NRC.Framework.BusinessLogic
 Public Class VendorDispositionCollection
     Inherits BusinessListBase(Of VendorDispositionCollection, VendorDisposition)
 
-    Public Function GetByVendorDispositionCode(ByVal dispositionCode As String) As VendorDisposition
+    Public Function GetByVendorDispositionCode(ByVal dispositionCode As String, ByVal isFinal As Boolean) As VendorDisposition
 
         For Each dispo As VendorDisposition In Me
-            If dispo.VendorDispositionCode.ToLower = dispositionCode.ToLower Then
-                Return dispo
+            If dispo.isFinal = 1 Then
+                If dispo.VendorDispositionCode.ToLower = dispositionCode.ToLower And isFinal Then
+                    Return dispo
+                End If
+            ElseIf dispo.isFinal = 0 Then
+                If dispo.VendorDispositionCode.ToLower = dispositionCode.ToLower And Not isFinal Then
+                    Return dispo
+                End If
+            Else 'dispo.isFinal = 9 ==> don't care
+                If dispo.VendorDispositionCode.ToLower = dispositionCode.ToLower Then
+                    Return dispo
+                End If
             End If
         Next
 
