3350be424e135bbe21b66bb6465ca11d74fbf7b3
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2014-09-29 12:49:37 -0500

    S9 US5 T1 - addt'l checking before deleting

--- Sprint_docs/Sprint9_docs/S9.US5.T1_Remove_PCMH_DefaultDQRules.sql
+++ Sprint_docs/Sprint9_docs/S9.US5.T1_Remove_PCMH_DefaultDQRules.sql
@@ -18,33 +18,47 @@ from surveytypesubtype stst
 inner join subtype st on st.subtype_id = stst.subtype_id
 where st.Subtype_nm = 'PCMH'
 
+if exists (select * from survey_def where surveytype_id = @Surveytypeid and survey_id in (select survey_id from surveysubtype where subtype_id=@Subtype_id))
+	print 'There are still surveys that use the PCMH subtype. Aborting'
+else
+begin
 
-delete
-from DefaultCriteriaStmt
-where DefaultCriteriaStmt_id in (
-	select DefaultCriteriaStmt_id
+	/* dbg: I don't think we want to do this. Some of these DefaultCriteriaStmt's are used for other survey/subtypes as well.
+	delete
+	from DefaultCriteriaStmt
+	where DefaultCriteriaStmt_id in (
+		select DefaultCriteriaStmt_id
+		from SurveyTypeDefaultCriteria
+		where surveytype_id = @SurveyTypeid
+		and Subtype_id = @Subtype_Id
+	)
+
+	delete
+	from DefaultCriteriaClause
+	where DefaultCriteriaStmt_id in (
+		select DefaultCriteriaStmt_id
+		from SurveyTypeDefaultCriteria
+		where surveytype_id = @SurveyTypeid
+		and Subtype_id = @Subtype_Id
+	)
+	*/
+
+	/* dbg: first we want to get rid of the PCMH specific default criteria statements: */
+	delete
 	from SurveyTypeDefaultCriteria
 	where surveytype_id = @SurveyTypeid
 	and Subtype_id = @Subtype_Id
-)
 
-delete
-from DefaultCriteriaClause
-where DefaultCriteriaStmt_id in (
-	select DefaultCriteriaStmt_id
-	from SurveyTypeDefaultCriteria
-	where surveytype_id = @SurveyTypeid
-	and Subtype_id = @Subtype_Id
-)
+	/* dbg: and then we want to delete any records in DefaultCriteriaStmt and DefaultCriteriaClause that are no longer referenced by any survey types/subtypes */
+	delete
+	from defaultcriteriastmt 
+	where DefaultCriteriaStmt_id not in (select DefaultCriteriaStmt_id from SurveyTypeDefaultCriteria)
 
+	delete
+	from DefaultCriteriaClause
+	where DefaultCriteriaStmt_id not in (select DefaultCriteriaStmt_id from SurveyTypeDefaultCriteria)
 
-
-delete
-from SurveyTypeDefaultCriteria
-where surveytype_id = @SurveyTypeid
-and Subtype_id = @Subtype_Id
-
-
+end
 
 
 /*
