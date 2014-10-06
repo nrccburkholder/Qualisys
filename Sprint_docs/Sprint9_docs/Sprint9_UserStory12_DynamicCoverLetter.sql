f939dc7923a6d207c9a7dcb40e7eff92cf1620ea
Chris Burkholder <cburkholder@nationalresearch.com>
2014-10-06 09:33:17 -0500

    S10 US4.1,4.2 text box fix, cover letter naming
    Companion update to FormLayout 3.9

--- Sprint_docs/Sprint9_docs/Sprint9_UserStory12_DynamicCoverLetter.sql
+++ Sprint_docs/Sprint9_docs/Sprint9_UserStory12_DynamicCoverLetter.sql
@@ -35,8 +35,8 @@ DEFAULT ''
 
 GO
 
-update QualPro_Params set strparam_value = '3.8' where STRPARAM_NM = 'FormLayoutVersion'
-					 and strparam_value <> '3.8'
+update QualPro_Params set strparam_value = '3.9' where STRPARAM_NM = 'FormLayoutVersion'
+					 and strparam_value <> '3.9'
 --update QualPro_Params set strparam_value = '3.0' 
 --select * from Qualpro_params where STRPARAM_NM = 'FormLayoutVersion'
 
