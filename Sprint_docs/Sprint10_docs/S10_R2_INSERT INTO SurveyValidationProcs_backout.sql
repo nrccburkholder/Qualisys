9a09b0bcb91e6b8ac42d2757a06d953511a97b19
tbutler <tbutler@nationalresearch.com>
2014-10-15 13:55:50 -0500

    S10 R2 Survey Validation refactoring

---
+++ Sprint_docs/Sprint10_docs/S10_R2_INSERT INTO SurveyValidationProcs_backout.sql
@@ -0,0 +1,32 @@
+
+/*
+Inserts new SurveyValidationProcs records 
+and the SurveyValidationProcsBySurveyType mappings 
+
+*/
+
+
+begin tran
+
+
+update [dbo].[SurveyValidationProcs]
+	set Active = 1
+where ProcedureName in (
+'SV_CAHPS_HH_CAHPS_DQRules'
+,'SV_CAHPS_H_CAHPS_DQRules'
+,'SV_CAHPS_PCMH_CAHPS_DQRules'
+)
+
+update [dbo].[SurveyValidationProcs]
+	set Active = 0
+where ProcedureName in (
+'SV_CAHPS_DQRules'
+)
+
+select *
+from SurveyValidationProcs_view
+
+
+commit tran
+
+ GO
