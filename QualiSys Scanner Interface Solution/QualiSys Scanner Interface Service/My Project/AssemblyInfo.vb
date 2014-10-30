1e1c250f4239e76d1b66ee3d97f8bc7af9f3abe1
tbutler <tbutler@nationalresearch.com>
2014-10-30 14:33:07 -0500

    QSI Services -- version number update

--- QualiSys Scanner Interface Solution/QualiSys Scanner Interface Service/My Project/AssemblyInfo.vb
+++ QualiSys Scanner Interface Solution/QualiSys Scanner Interface Service/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.28.3.0")> 
-<Assembly: AssemblyFileVersion("1.28.3.0")> 
+<Assembly: AssemblyVersion("1.28.4.0")> 
+<Assembly: AssemblyFileVersion("1.28.4.0")> 
