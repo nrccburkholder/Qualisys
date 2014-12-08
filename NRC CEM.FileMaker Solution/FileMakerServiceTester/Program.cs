aafcd0d8d212db985551ce6450ec9aef36cfb7c1
tbutler <tbutler@nationalresearch.com>
2014-12-08 10:42:39 -0600

    S14 NRC CEM.FileMaker Solution - add testing app

---
+++ NRC CEM.FileMaker Solution/FileMakerServiceTester/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceLogging;
+using NRC.Exporting;
+
+namespace FileMakerServiceTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press return to start");
+            Console.ReadLine();
+
+            Start();
+
+            Console.WriteLine("Press return to exit");
+            Console.ReadLine();
+
+        }
+
+        static void Start()
+        {
+            NRC.Exporting.FileMakerServiceWorker.Run();
+        }
+    }
+}
