f81edf7e71b0597f2d069be67599bddd61b5884a
Chris Burkholder <cburkholder@nationalresearch.com>
2014-12-19 09:41:04 -0600

    S14B US9 FormLayout companion update 3.26
    Are you sure? for Questions/Sections companion update

---
+++ Sprint_docs/Sprint14_2_docs/S14.2_US9_FormLayout_VersionUpdateOnly.sql
@@ -0,0 +1,19 @@
+/*
+S13.US7	As a Formlayout user, I need better management of translation for text boxes	
+		Setup the Implementation Client from the template to get expected variations of cover letters	
+
+7.1	Add dynamic cover letters to print mockup
+
+Chris Burkholder
+
+Only updating QualPro_Param
+*/
+
+use [QP_Prod]
+
+update QualPro_Params set strparam_value = '3.26' where STRPARAM_NM = 'FormLayoutVersion'
+					 and strparam_value <> '3.26'
+--update QualPro_Params set strparam_value = '3.0' 
+--select * from Qualpro_params where STRPARAM_NM = 'FormLayoutVersion'
+
+GO
