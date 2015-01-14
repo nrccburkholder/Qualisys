824a3efda48753c1136036596ab2c8a62e367415
tbutler <tbutler@nationalresearch.com>
2015-01-14 15:45:58 -0600

    CEM/NRC_DataMart SampleSet.StandardMethodlogyID

---
+++ Sprint_docs/Sprint15_docs/S15_US11.2_CatalystETL_Update - Copy.sql
@@ -0,0 +1,27 @@
+/*
+
+	S14.2 US11.2 Add mailing methodology to sample set table in NRC datamart and assosiated stored procs.
+
+	Tim Butler
+
+	alter table [dbo].[SampleSetTemp]
+
+*/
+
+use [NRC_DataMart_ETL]
+go
+begin tran
+go
+if not exists (	SELECT 1 
+				FROM   sys.tables st 
+					   INNER JOIN sys.columns sc ON st.object_id = sc.object_id 
+				WHERE  st.schema_id = 1 
+					   AND st.NAME = 'SampleSetTemp' 
+					   AND sc.NAME = 'StandardMethodologyID' )
+
+	alter table [dbo].[SampleSetTemp] add StandardMethodologyID tinyint NOT NULL default(0)
+
+go
+
+commit tran
+go
