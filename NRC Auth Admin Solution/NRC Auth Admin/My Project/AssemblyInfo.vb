f3ee1ec5e9139dc06de86ef886288969f31f652b
tbutler <tbutler@nationalresearch.com>
2015-01-21 13:48:38 -0600

    S16 US16 CIHI survey setup

--- NRC Auth Admin Solution/NRC Auth Admin/My Project/AssemblyInfo.vb
+++ NRC Auth Admin Solution/NRC Auth Admin/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("National Research Corporation")> 
 <Assembly: AssemblyProduct("NRC Auth Administration")> 
-<Assembly: AssemblyCopyright("Copyright ©  2006")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.1.2")> 
-<Assembly: AssemblyFileVersion("1.2.1.2")> 
+<Assembly: AssemblyVersion("1.2.6.0")> 
+<Assembly: AssemblyFileVersion("1.2.6.0")> 
