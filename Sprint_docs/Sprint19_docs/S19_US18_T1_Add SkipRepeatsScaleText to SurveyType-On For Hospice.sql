051cdec8abe92334b2a5cafa08311140f04d0c69
Chris Burkholder <cburkholder@nationalresearch.com>
2015-02-24 14:00:20 -0600

    S19_US18_T1_Add SkipRepeatsScaleText to SurveyType
    -On For Hospice

---
+++ Sprint_docs/Sprint19_docs/S19_US18_T1_Add SkipRepeatsScaleText to SurveyType-On For Hospice.sql
@@ -0,0 +1,73 @@
+/*
+CJB 2/24/2015
+
+Modify Skip Language	
+
+Analyze the method and time needed for various options. 	
+
+Spanish as well. Probably less than 20 hours to put a flag in SurveyType table. Double check to see how the text wrap works. 
+
+18.1	Code Change in shared code. Add field to SurveyType Table. Create Read proc. 
+
+*/
+
+/*
+select * from surveytype
+
+sp_helptext QCL_selectallsurveytypes
+
+sp_helptext QCL_selectsurveytype
+
+*/
+
+begin tran
+
+IF NOT EXISTS (
+            SELECT 1
+            FROM INFORMATION_SCHEMA.COLUMNS
+            WHERE TABLE_NAME = 'SurveyType'
+              AND COLUMN_NAME = 'SkipRepeatsScaleText'
+)
+ALTER TABLE SurveyType 
+ADD SkipRepeatsScaleText bit NOT NULL
+DEFAULT 0
+
+GO
+
+update SurveyType set SkipRepeatsScaleText = 1 where SurveyType_dsc = 'Hospice CAHPS' and SkipRepeatsScaleText = 0
+
+GO
+
+ALTER PROCEDURE [dbo].[QCL_SelectAllSurveyTypes]
+AS
+
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+SELECT SurveyType_ID, SurveyType_dsc, CAHPSType_id, SeedMailings, SeedSurveyPercent, SeedUnitField, SkipRepeatsScaleText
+FROM [dbo].SurveyType
+
+SET NOCOUNT OFF
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+GO
+
+ALTER PROCEDURE [dbo].[QCL_SelectSurveyType]
+    @SurveyType_ID INT
+AS
+
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+SELECT SurveyType_ID, SurveyType_dsc, CAHPSType_id, SeedMailings, SeedSurveyPercent, SeedUnitField, SkipRepeatsScaleText
+FROM [dbo].SurveyType
+WHERE SurveyType_ID = @SurveyType_ID
+
+SET NOCOUNT OFF
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+
+GO
+
+commit tran
+
+GO
