9f7284c5b61f818bcdbfb0d468fce318a31caf46
tbutler <tbutler@nationalresearch.com>
2015-02-26 09:26:41 -0600

    S19_US20 update SV_CAHPS_ActiveMethodology to check for expired standardmethodologies

--- Sprint_docs/Sprint19_docs/S19_US20_ICH_Methodologies.sql
+++ Sprint_docs/Sprint19_docs/S19_US20_ICH_Methodologies.sql
@@ -7,7 +7,7 @@ Tim Butler
 20.1	alter table [dbo].[StandardMethodologybySurveyType] add expired (QP_PROD)
 20.2	Insert new methodologies
 20.3	ALTER PROCEDURE [dbo].[QCL_SelectStandardMethodologiesBySurveyTypeId]
-
+20.4	ALTER PROCEDURE [dbo].[SV_CAHPS_ActiveMethodology]
 */
 
 
@@ -236,3 +236,146 @@ SET NOCOUNT OFF
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED
 
 GO
+
+USE [QP_Prod]
+GO
+/****** Object:  StoredProcedure [dbo].[SV_CAHPS_ActiveMethodology]    Script Date: 2/25/2015 4:11:50 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[SV_CAHPS_ActiveMethodology]
+    @Survey_id INT
+AS
+
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+SET NOCOUNT ON
+
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+SET NOCOUNT ON
+
+/*
+HCAHPS IP			2
+Home Health CAHPS	3
+CGCAHPS				4
+ICHCAHPS			8
+ACOCAHPS			10
+*/
+
+-- CAHPS surveyType_id 'constants'
+declare @CGCAHPS int
+SELECT @CGCAHPS = SurveyType_Id from SurveyType where SurveyType_dsc = 'CGCAHPS'
+
+declare @HCAHPS int
+SELECT @HCAHPS = SurveyType_Id from SurveyType where SurveyType_dsc = 'HCAHPS IP'
+
+declare @HHCAHPS int
+SELECT @HHCAHPS = SurveyType_Id from SurveyType where SurveyType_dsc = 'Home Health CAHPS'
+
+declare @ACOCAHPS int
+SELECT @ACOCAHPS = SurveyType_Id from SurveyType where SurveyType_dsc = 'ACOCAHPS'
+
+declare @ICHCAHPS int
+SELECT @ICHCAHPS = SurveyType_Id from SurveyType where SurveyType_dsc = 'ICHCAHPS'
+
+declare @hospiceCAHPS int
+SELECT @hospiceCAHPS = SurveyType_Id from SurveyType where SurveyType_dsc = 'Hospice CAHPS'
+
+declare @PCMHSubType int
+SELECT @PCMHSubType = 9
+
+declare @CIHI int
+select @CIHI = SurveyType_Id from SurveyType where SurveyType_dsc = 'CIHI CPES-IC'
+
+declare @surveyType_id int
+declare @subtype_id int
+
+SELECT  @surveyType_id = SurveyType_id
+from SURVEY_DEF
+where SURVEY_ID = @Survey_id
+
+-- only going to get subtypes where there is a bitOverride set, otherwise we'll just use the SurveyType validation
+select @subtype_id = sst.subtype_id 
+from SurveySubtype sst
+inner join Subtype st on st.Subtype_id = sst.Subtype_id
+where survey_id = @Survey_id
+and st.SubtypeCategory_id = 1
+and st.bitRuleOverride = 1
+
+
+
+IF @SurveyType_id not in (select SurveyType_ID from surveytype where CAHPSType_id is not null)
+BEGIN
+    RETURN
+END
+
+DECLARE @SurveyTypeDescription varchar(20)
+
+
+if @SubType_id is null
+BEGIN
+	SELECT @SurveyTypeDescription = [SurveyType_dsc]
+	FROM [dbo].[SurveyType] 
+	WHERE SurveyType_ID = @surveyType_id
+END
+ELSE
+BEGIN
+	SELECT @SurveyTypeDescription = SubType_nm
+	FROM [dbo].[Subtype]
+	WHERE SubType_id = @subtype_id
+END
+
+IF @subtype_id is null
+	SET @subtype_id = 0
+
+declare @study_ID int, @EncTable_ID int
+Select @study_ID = study_ID from SURVEY_DEF where SURVEY_ID = @Survey_id
+
+--Need a temp table to store the messages.  We will select them at the end.
+--0=Passed,1=Error,2=Warning
+CREATE TABLE #M (Error TINYINT, strMessage VARCHAR(200))
+
+--Check the active methodology  (ALL CAHPS)
+CREATE TABLE #ActiveMethodology (standardmethodologyid INT)
+
+INSERT INTO #ActiveMethodology
+SELECT mm.StandardMethodologyId, smst.bitExpired
+FROM MailingMethodology mm (NOLOCK)
+INNER JOIN StandardMethodology sm ON (sm.StandardMethodologyID = mm.StandardMethodologyID)
+INNER JOIN StandardMethodologyBySurveyType smst ON smst.StandardMethodologyID=sm.StandardMethodologyID
+WHERE mm.Survey_id=@Survey_id
+AND mm.bitActiveMethodology=1
+
+IF @@ROWCOUNT<>1
+ INSERT INTO #M (Error, strMessage)
+ SELECT 1,'Survey must have exactly one active methodology.'
+ELSE
+BEGIN
+
+	 IF EXISTS(SELECT * FROM #ActiveMethodology WHERE standardmethodologyid = 5 and bitExpired = 0) -- 5 is custom methodology
+	  INSERT INTO #M (Error, strMessage)
+	  SELECT 2,'Survey uses a custom methodology.'         -- a warning
+
+	 ELSE IF EXISTS(SELECT * FROM #ActiveMethodology WHERE bitExpired = 1) -- 
+	  INSERT INTO #M (Error, strMessage)
+	  SELECT 1,'Survey uses an expired methodology.'         -- an error
+
+	 ELSE IF EXISTS(SELECT * FROM #ActiveMethodology
+	   WHERE standardmethodologyid in (select StandardMethodologyID
+		 from StandardMethodologyBySurveyType where SurveyType_id = @surveyType_id and SubType_ID = @subtype_id
+		)
+	   )
+	  INSERT INTO #M (Error, strMessage)
+	  SELECT 0,'Survey uses a standard ' + @SurveyTypeDescription + ' methodology.'
+
+	 ELSE
+	  INSERT INTO #M (Error, strMessage)
+	  SELECT 1,'Survey does not use a standard ' + @SurveyTypeDescription + ' methodology.'   -- a warning     
+
+END
+
+DROP TABLE #ActiveMethodology
+
+SELECT * FROM #M
+
+DROP TABLE #M
