ed9e9aa6927b47fbe74f113d0faeae39a169f03a
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2015-02-27 15:05:34 -0600

    S19.US14 - had addr & phone codes in the wrong places

--- Sprint_docs/Sprint19_docs/S19_US14_sp_phase3_questionresult_for_extract.sql
+++ Sprint_docs/Sprint19_docs/S19_US14_sp_phase3_questionresult_for_extract.sql
@@ -599,13 +599,13 @@ INSERT INTO drm_tracktimes
 	update dl set disposition_id=46, LoggedBy = left(LoggedBy + ',sp_phase3_questionresult_for_extract',42)
 	from #Hospice h
     inner join dispositionlog dl on h.samplepop_id=dl.samplepop_id
-	where h.BadAddr=1 and h.BadPhone=0 and dl.disposition_id in (14,16)
+	where h.BadAddr=1 and h.BadPhone=0 and dl.disposition_id in (5)
 
 	-- if there's a bad phone, but no bad addr - change the bad phone disposition(s) to "unused bad phone"
 	update dl set disposition_id=47, LoggedBy = left(LoggedBy + ',sp_phase3_questionresult_for_extract',42)
 	from #Hospice h
     inner join dispositionlog dl on h.samplepop_id=dl.samplepop_id
-	where h.BadAddr=0 and h.BadPhone=1 and dl.disposition_id in (5)
+	where h.BadAddr=0 and h.BadPhone=1 and dl.disposition_id in (14,16)
 	
 	-- if there's both bad phone and bad addr - change any previously Unused dispositions back to used
 	update dl set disposition_id=case when dl.disposition_id=46 then 5 else 14 end
