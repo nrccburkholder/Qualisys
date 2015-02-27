5d6d59ec8a45748e8ad12861ba9fa7ff37e1ab0d
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2015-02-27 14:59:17 -0600

    S19.US14 - added update to DispositionLog.LoggedBy field

--- Sprint_docs/Sprint19_docs/S19_US14_sp_phase3_questionresult_for_extract.sql
+++ Sprint_docs/Sprint19_docs/S19_US14_sp_phase3_questionresult_for_extract.sql
@@ -596,13 +596,13 @@ INSERT INTO drm_tracktimes
     group by cqw.samplepop_id
 
 	-- if there's a bad address, but no bad phone - change the bad addr disposition(s) to "unused bad addr"
-	update dl set disposition_id=46
+	update dl set disposition_id=46, LoggedBy = left(LoggedBy + ',sp_phase3_questionresult_for_extract',42)
 	from #Hospice h
     inner join dispositionlog dl on h.samplepop_id=dl.samplepop_id
 	where h.BadAddr=1 and h.BadPhone=0 and dl.disposition_id in (14,16)
 
 	-- if there's a bad phone, but no bad addr - change the bad phone disposition(s) to "unused bad phone"
-	update dl set disposition_id=47
+	update dl set disposition_id=47, LoggedBy = left(LoggedBy + ',sp_phase3_questionresult_for_extract',42)
 	from #Hospice h
     inner join dispositionlog dl on h.samplepop_id=dl.samplepop_id
 	where h.BadAddr=0 and h.BadPhone=1 and dl.disposition_id in (5)
