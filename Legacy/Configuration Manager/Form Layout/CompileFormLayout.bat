877f7a7e57d5fbada63f1e172eb0b5c97af17069
Chris Burkholder <CBurkholder@NationalResearch.com>
2015-03-03 12:19:43 -0600

    S19 US2 T4 Hooked up Delphi 3 command line compile
    CompileFormLayout.bat
    CompilePCLGen.bat

---
+++ Legacy/Configuration Manager/Form Layout/CompileFormLayout.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Borland\Delphi 3\BIN\dcc32" Dynaquest.dpr -B -U"..\..\delphi 3\ip3\lib;..\..\delphi 3\qualpro;..\..\delphi 3\edsvcl;..\..\delphi 3\pp;..\..\Delphi Shared Code\QFP;..\..\Delphi Shared Code\FP;..\..\Delphi Shared Code\QF;..\..\PCL Gen\lib" -DFormLayout
