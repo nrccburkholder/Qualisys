f276d6b71024acc8302dbca2792ec8b474a648f1
tbutler <tbutler@nationalresearch.com>
2015-04-23 11:39:45 -0500

    S19 US21 CIHI Dummy File Submission

---
+++ NRC CEM.FileMaker Solution/CIHI Submission File Stuff/Catalyst.DataMart.Library/ExportColumnResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalyst.DataMart.Library
+{
+    public class ExportColumnResponse
+    {
+
+        public int ExportTemplateColumnResponseID { get; set; }
+        public int ExportTemplateColumnId { get; set; }
+        public int RawValue { get; set; }
+        public string ExportColumnName { get; set; }
+        public string RecodeValue { get; set; }
+        public string ResponseLabel { get; set; }
+
+
+
+        public ExportColumnResponse()
+        {
+
+        }
+
+
+    }
+}
---
+++ NRC CEM.FileMaker Solution/CIHI Submission File Stuff/Catalyst.DataMart.Library/ExportSection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalyst.DataMart.Library
+{
+    public class ExportSection
+    {
+
+        public int ExportTemplateSectionID{ get; set; }
+        public string ExportTemplateSectionName{ get; set; }
+        public int ExportTemplateID{ get; set; }
+        public string DefaultNamingConvention{ get; set; }
+        public List<ExportColumn> ExportColumns{ get; set; }
+
+        public ExportSection()
+        {
+            ExportColumns = new List<ExportColumn>();
+        }
+
+    }
+}
