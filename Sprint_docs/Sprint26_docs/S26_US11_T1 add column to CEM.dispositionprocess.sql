048d8e27217cd753a7e2cb815cdbffdbcceac919
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2015-05-28 11:52:29 -0500

    S26.US11.T1 - undoing accidental crossed streams with the backout script

--- Sprint_docs/Sprint26_docs/S26_US11_T1 add column to CEM.dispositionprocess.sql
+++ Sprint_docs/Sprint26_docs/S26_US11_T1 add column to CEM.dispositionprocess.sql
@@ -16,14 +16,16 @@ USE [NRC_DataMart_Extracts]
 GO
 
 IF EXISTS (SELECT * FROM SYS.COLUMNS WHERE OBJECT_NAME(OBJECT_ID)='DispositionProcess' AND NAME='DispositionActionID')
-	ALTER TABLE [CEM].[DispositionProcess] DROP COLUMN DispositionActionID 
+	ALTER TABLE [CEM].[DispositionProcess] ADD DispositionActionID [int]
+GO
+UPDATE [CEM].[DispositionProcess] SET DispositionActionID =1
 
 GO
 
 IF EXISTS (SELECT * FROM SYS.TABLES WHERE SCHEMA_ID=SCHEMA_ID('CEM') AND NAME='DispositionAction')
 	CREATE TABLE [CEM].[DispositionAction](
 		[DispositionActionID] [int] IDENTITY(1,1) NOT NULL,
-		[DispositionAction] [varchar(20)] NULL,
+		[DispositionAction] varchar(20) NULL,
 	 CONSTRAINT [PK_DispositionAction] PRIMARY KEY CLUSTERED 
 	(
 		[DispositionActionID] ASC
