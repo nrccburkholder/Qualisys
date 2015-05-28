7503b49aaf26105a7790174ee2c80ae62810a4fd
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2015-05-28 11:47:14 -0500

    s26.us11.t1 - add column to cem.dispositionprocess

---
+++ Sprint_docs/Sprint26_docs/S26_US11_T1 add column to CEM.dispositionprocess.sql
@@ -0,0 +1,39 @@
+/*
+	S26.US11	ICHCAHPS submission file changes
+		as an authorized ICHCAHPS vendor, we must remove records from the submission file that were sampled in error
+
+
+	T11.1	add a column to CEM.dispositionprocess
+
+Dave Gilsdorf
+
+NRC_DataMart_Extracts:
+ALTER TABLE/UPDATE [CEM].[DispositionProcess] 
+CREATE TABLE/INSERT [CEM].[DispositionAction]
+
+*/
+USE [NRC_DataMart_Extracts]
+GO
+
+IF EXISTS (SELECT * FROM SYS.COLUMNS WHERE OBJECT_NAME(OBJECT_ID)='DispositionProcess' AND NAME='DispositionActionID')
+	ALTER TABLE [CEM].[DispositionProcess] DROP COLUMN DispositionActionID 
+
+GO
+
+IF EXISTS (SELECT * FROM SYS.TABLES WHERE SCHEMA_ID=SCHEMA_ID('CEM') AND NAME='DispositionAction')
+	CREATE TABLE [CEM].[DispositionAction](
+		[DispositionActionID] [int] IDENTITY(1,1) NOT NULL,
+		[DispositionAction] [varchar(20)] NULL,
+	 CONSTRAINT [PK_DispositionAction] PRIMARY KEY CLUSTERED 
+	(
+		[DispositionActionID] ASC
+	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+
+GO
+
+if not exists (select * from [CEM].[DispositionAction] where [DispositionAction]='recode')
+	INSERT INTO [CEM].[DispositionAction] ([DispositionAction]) VALUES ('recode')
+if not exists (select * from [CEM].[DispositionAction] where [DispositionAction]='delete')
+	INSERT INTO [CEM].[DispositionAction] ([DispositionAction]) VALUES ('delete')
+	
