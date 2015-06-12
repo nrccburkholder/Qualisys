4120912b71b775f6d088c74ae5720d5ec0524400
tbutler <tbutler@nationalresearch.com>
2015-06-12 12:21:44 -0500

    Sprint 27 Production Release -- Database Objects update

--- Databases/QP_Prod/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/QCL_SelectSubTypes.proc.sql
+++ Databases/QP_Prod/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/QCL_SelectSubTypes.proc.sql
@@ -1,28 +1,43 @@
-USE [QP_Prod]
-GO
 
-/****** Object:  StoredProcedure [dbo].[QCL_SelectSubTypes]    Script Date: 8/19/2014 10:55:06 AM ******/
+
+
+/****** Object:  StoredProcedure [dbo].[QCL_SelectSubTypes]    Script Date: 4/14/2015 2:05:38 PM ******/
 SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE PROCEDURE [dbo].[QCL_SelectSubTypes] 
 @surveytypeid int,
 @subtypecategory int,
 @surveyid int 
 AS
 
-SELECT distinct st.SubType_id, SurveyType_ID, SubType_NM, stc.SubtypeCategory_id, stc.SubtypeCategory_nm, stc.bitMultiSelect, st.bitRuleOverride, CASE WHEN sst.Subtype_id IS NULL THEN 0 ELSE 1 END bitSelected
-FROM SurveyTypeSubType stst
-inner join Subtype st on (st.Subtype_id = stst.Subtype_id)
-inner join SubtypeCategory stc on (stc.SubtypeCategory_id = st.SubtypeCategory_id)
-left join SurveySubtype sst on (sst.Subtype_id = st.Subtype_id and sst.Survey_id = @surveyid)
-WHERE SurveyType_ID = @surveytypeid
-and stc.SubtypeCategory_id = @subtypecategory
+/*
+	S23_US13 Tim Butler -- changed to join with SubtypeSubtypeMapping
+*/
 
+if @SubtypeCategory = 1
+-- this returns records for loading the SubType combobox
+	SELECT distinct st.SubType_id, SurveyType_ID, SubType_NM, stc.SubtypeCategory_id, stc.SubtypeCategory_nm, stc.bitMultiSelect, st.bitRuleOverride, CASE WHEN sst.Subtype_id IS NULL THEN 0 ELSE 1 END bitSelected, ISNULL(ststm.ParentSubtype_id,0) ParentSubType_id, st.bitQuestionnaireRequired
+	FROM SurveyTypeSubType stst
+	inner join Subtype st on (st.Subtype_id = stst.Subtype_id)
+	inner join SubtypeCategory stc on (stc.SubtypeCategory_id = st.SubtypeCategory_id)
+	left join SurveySubtype sst on (sst.Subtype_id = st.Subtype_id and sst.Survey_id = @surveyid)
+	left join SubtypeSubtypeMapping ststm on (ststm.ParentSubtype_id = st.Subtype_id)  -- joins on ParentSubType_id
+	WHERE SurveyType_ID = @surveytypeid
+	and stc.SubtypeCategory_id = @SubtypeCategory
+
+ELSE if @SubtypeCategory = 2
+-- this returns records for loading the Questionnaire combobox
+	SELECT distinct st.SubType_id, SurveyType_ID, SubType_NM, stc.SubtypeCategory_id, stc.SubtypeCategory_nm, stc.bitMultiSelect, st.bitRuleOverride, CASE WHEN sst.Subtype_id IS NULL THEN 0 ELSE 1 END bitSelected, ISNULL(ststm.ParentSubtype_id,0) ParentSubType_id, st.bitQuestionnaireRequired
+	--INTO #QuestionnaireTypes
+	FROM SurveyTypeSubType stst
+	inner join Subtype st on (st.Subtype_id = stst.Subtype_id)
+	inner join SubtypeCategory stc on (stc.SubtypeCategory_id = st.SubtypeCategory_id)
+	left join SurveySubtype sst on (sst.Subtype_id = st.Subtype_id and sst.Survey_id = @surveyid)
+	left join SubtypeSubtypeMapping ststm on (ststm.ChildSubtype_id = st.Subtype_id)  -- joins on ChildSubType_id
+	WHERE SurveyType_ID = @surveytypeid
+	and stc.SubtypeCategory_id = @SubtypeCategory
 
 GO
-
 
--- Databases/QP_Prod/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SV_CAHPS_MedicareNumber.proc.sql
+++ Databases/QP_Prod/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/SV_CAHPS_MedicareNumber.proc.sql
@@ -1,50 +1,20 @@
-USE [QP_Prod]
-GO
 
-/****** Object:  StoredProcedure [dbo].[SV_CAHPS_MedicareNumber]    Script Date: 8/13/2014 1:41:12 PM ******/
-SET ANSI_NULLS ON
-GO
 
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE PROCEDURE [dbo].[SV_CAHPS_MedicareNumber]
+ALTER PROCEDURE [dbo].[SV_CAHPS_MedicareNumber]
     @Survey_id INT
 AS
 
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
 SET NOCOUNT ON
 
-SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
-SET NOCOUNT ON
-
-/*
-HCAHPS IP			2
-Home Health CAHPS	3
-CGCAHPS				4
-ICHCAHPS			8
-ACOCAHPS			10
-*/
-
--- CAHPS surveyType_id 'constants'
-declare @CGCAHPS int
-SET @CGCAHPS = 4
-
 declare @HCAHPS int
-SET @HCAHPS = 2
+SELECT @HCAHPS = surveytype_id
+from SurveyType
+where SurveyType_dsc='HCAHPS IP'
 
-declare @HHCAHPS int
-SET @HHCAHPS = 3
+declare @surveyType_id int, @Study_id int
 
-declare @ACOCAHPS int
-SET @ACOCAHPS = 10
-
-declare @ICHCAHPS int
-SET @ICHCAHPS = 8
-
-declare @surveyType_id int
-
-SELECT  @surveyType_id = SurveyType_id
+SELECT @surveyType_id = SurveyType_id, @Study_id = Study_id
 from SURVEY_DEF
 where SURVEY_ID = @Survey_id
 
@@ -59,46 +29,96 @@ SELECT @SurveyTypeDescription = [SurveyType_dsc]
 FROM [dbo].[SurveyType] 
 WHERE SurveyType_ID = @surveyType_id
 
-
-declare @study_ID int, @EncTable_ID int
-Select @study_ID = study_ID from SURVEY_DEF where SURVEY_ID = @Survey_id
-
 --Need a temp table to store the messages.  We will select them at the end.
 --0=Passed,1=Error,2=Warning
 CREATE TABLE #M (Error TINYINT, strMessage VARCHAR(200))
 
 --Make sure the Medicare number is populated.
 INSERT INTO #M (Error, strMessage)
-SELECT 1,'Medicare number is not populated.'
-FROM SamplePlan sp, SampleUnit su LEFT JOIN SUFacility suf
-ON su.SUFacility_id=suf.SUFacility_id
-WHERE sp.Survey_id=@Survey_id
-AND sp.SamplePlan_id=su.SamplePlan_id
-AND su.CAHPSType_id = @surveyType_id
-AND (suf.MedicareNumber IS NULL
-OR LTRIM(RTRIM(suf.MedicareNumber))='')
-IF @@ROWCOUNT=0
-INSERT INTO #M (Error, strMessage)
-SELECT 0,'Medicare number is populated'
+SELECT 1, 'Medicare number is not populated.' 
+FROM   sampleplan sp, 
+       sampleunit su 
+       LEFT JOIN sufacility suf ON su.sufacility_id = suf.sufacility_id 
+WHERE  sp.survey_id = @Survey_id 
+       AND sp.sampleplan_id = su.sampleplan_id 
+       AND su.cahpstype_id = @surveyType_id 
+       AND ( suf.medicarenumber IS NULL 
+              OR Ltrim(Rtrim(suf.medicarenumber)) = '' ) 
+
+IF @@ROWCOUNT = 0 
+  INSERT INTO #M (Error, strMessage)
+  SELECT 0,'Medicare number is populated'
 
 --make sure the Medicare number is active
 INSERT INTO #M (Error, strMessage)
-SELECT 1,'Medicare number is not Active'
-FROM SamplePlan sp, SampleUnit su,SUFacility suf, MedicareLookup ml
-WHERE sp.Survey_id=@Survey_id
-AND su.SUFacility_id=suf.SUFacility_id
-AND sp.SamplePlan_id=su.SamplePlan_id
-AND ml.MedicareNumber = suf.MedicareNumber
-AND su.CAHPSType_id = @surveyType_id
-AND ml.Active = 0
-IF @@ROWCOUNT=0
-INSERT INTO #M (Error, strMessage)
-SELECT 0,'Medicare number is Active'
+SELECT 1, 'Medicare number is not Active' 
+FROM   sampleplan sp, 
+       sampleunit su, 
+       sufacility suf, 
+       medicarelookup ml 
+WHERE  sp.survey_id = @Survey_id 
+       AND su.sufacility_id = suf.sufacility_id 
+       AND sp.sampleplan_id = su.sampleplan_id 
+       AND ml.medicarenumber = suf.medicarenumber 
+       AND su.cahpstype_id = @surveyType_id 
+       AND ml.active = 0 
 
+IF @@ROWCOUNT = 0 
+  INSERT INTO #m (error, strmessage) 
+  SELECT 0, 'Medicare number is Active' 
+
+IF @surveyType_id = @HCAHPS
+BEGIN
+	DECLARE @CCNList TABLE (MedicareNumber varchar(20))
+	INSERT INTO @CCNList (MedicareNumber)
+	SELECT DISTINCT suf.MedicareNumber
+	FROM dbo.SamplePlan sp
+	INNER JOIN dbo.SampleUnit su on sp.SamplePlan_id = su.SamplePlan_id
+	INNER JOIN dbo.SUFacility suf on su.SUFacility_id = suf.SUFacility_id
+	INNER JOIN dbo.MedicareLookup ml on SUF.MedicareNumber = ml.MedicareNumber
+	WHERE sp.Survey_id = @Survey_id
+	and ml.Active = 1
+	and su.bitHCAHPS = 1
+	and su.DontSampleUnit = 0
+
+	INSERT INTO #M (Error, strMessage)
+	SELECT distinct 1, 'CCN "'+ccn.MedicareNumber+'" is also used in study ' + convert(varchar,sd.Study_id) + ', survey ' + convert(varchar, sd.Survey_id)
+	FROM @CCNList ccn
+	INNER JOIN dbo.SUFacility suf on ccn.MedicareNumber = suf.MedicareNumber
+	INNER JOIN dbo.MedicareLookup ml on SUF.MedicareNumber = ml.MedicareNumber
+	INNER JOIN dbo.SampleUnit su on suf.SUFacility_id = su.SUFacility_id
+	INNER JOIN dbo.SamplePlan sp on su.SamplePlan_id=sp.SamplePlan_id
+	INNER JOIN dbo.Survey_def sd on sp.Survey_id = sd.Survey_id
+	WHERE ml.Active = 1
+	AND sd.Study_id <> @Study_id
+	and su.bitHCAHPS = 1
+	and su.DontSampleUnit = 0
+	
+	IF @@ROWCOUNT=0
+	BEGIN
+		INSERT INTO #m (error, strmessage) 
+		SELECT 0, 'All CCNs are unique to this study' 
+	END
+	ELSE
+	BEGIN
+		DECLARE @Client_id int, @ClientExceptionList VARCHAR(100)
+		SELECT @Client_id=client_id
+		from dbo.Study
+		where Study_id=@Study_id
+		
+		SELECT @ClientExceptionList = strParam_Value
+		FROM dbo.Qualpro_params
+		WHERE STRPARAM_NM = 'SV_CCN_Exceptions' 
+		AND strParam_Grp = 'ConfigurationManager' 
+		AND datParam_Value > getdate()
+		
+		IF EXISTS (SELECT items FROM dbo.split(@ClientExceptionList,',') where items=@Client_id)
+			UPDATE #M
+			SET Error = 2
+			where strMessage like 'CCN%'
+	END	
+END
 
 SELECT * FROM #M
 
 DROP TABLE #M
-GO
-
-
