fdb0753bacf5ce84d0b221b6c44490817a219ff9
tbutler <tbutler@nationalresearch.com>
2015-07-20 14:29:10 -0500

    New: S29 US31.4  Added Hospice Xml Exporter

---
+++ NRC CEM Exporting/NRC.Exporting/XMLWriter.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+using System.Data;
+using System.Xml.Schema;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace CEM.Exporting
+{
+
+    public class XMLWriter : IDisposable
+    {
+
+        #region properties
+
+        private XmlTextWriter writer;
+
+        private StringWriter mStringWriter;
+
+        private XmlTextWriter Writer { get { return writer; } }
+
+        public string XmlString
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                sb = mStringWriter.GetStringBuilder();
+                writer.Close();
+                return sb.ToString();
+            }
+        }
+
+        #endregion
+
+        #region public methods
+
+        public void StartElement(string name)
+        {
+            writer.WriteStartElement(name);
+        }
+
+        public void EndElement()
+        {
+            writer.WriteEndElement();
+        }
+
+        public void WriteAttribute(string name, string value)
+        {
+            if (name != null)
+            {
+                if (value != null)
+                {
+                    writer.WriteAttributeString(name, value);
+                }
+            }
+        }
+
+        public void WriteElementString(string name, string value)
+        {
+            if (name != null)
+            {
+                if (value != null)
+                {
+                    writer.WriteElementString(name, SanitizeElement(value));
+                }
+            }
+        }
+
+        public void WriteCommentstring(string comment)
+        {
+            writer.WriteComment(comment);
+        }        
+
+        private string SanitizeElement(string s)
+        {
+            string temp = s;
+
+            temp = temp.Replace("&", "&amp;");
+            temp = temp.Replace("<", "&lt;");
+            temp = temp.Replace(">", "&gt;");
+            temp = temp.Replace("'", "&apos;");
+            temp = temp.Replace(@"""", "&quot;");
+            temp = temp.Replace("\a", "");
+
+            return temp;
+        }
+
+        public void Flush()
+        {
+            writer.Flush();
+        }
+
+        public void Close()
+        {
+            writer.Close();
+        }
+
+        #endregion
+
+        #region constructors
+
+        public XMLWriter()
+        {
+            mStringWriter = new StringWriter();
+            writer = new XmlTextWriter(mStringWriter);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteRaw(@"<?xml version=""1.0"" encoding=""UTF-8""?>");
+        }
+
+        public XMLWriter(StreamWriter stream)
+        {
+            writer = new XmlTextWriter(stream);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteRaw(@"<?xml version=""1.0"" encoding=""UTF-8""?>");
+        }
+
+        public XMLWriter(StreamWriter stream, string stylesheet)
+        {
+            writer = new XmlTextWriter(stream);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteProcessingInstruction("xml-stylesheet", string.Format(" type='text/xsl' href='{0}'", stylesheet));
+        }
+
+        #endregion
+
+        #region destructors
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                writer.Dispose();
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ NRC CEM Exporting/NRC.Exporting/XmlExporters/XMLExporter_Hospice.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+using System.IO;
+using System.Data;
+
+namespace CEM.Exporting.XmlExporters
+{
+    public class XMLExporter_Hospice: BaseXmlExporter
+    {
+
+        #region private members
+
+        #endregion
+
+        #region constructors
+
+        public XMLExporter_Hospice(): base()
+        {
+
+        }
+
+        #endregion
+
+
+        public override XmlDocumentEx MakeExportXMLDocument(List<ExportDataSet> ds, ExportTemplate template)
+        {
+
+            if (ds.Count > 0)
+            {
+                Encoding encoding = new UTF8Encoding(false);
+
+                XmlSchema schema = XmlSchema.Read(new StringReader(template.XMLSchemaDefinition), null);
+                schema.Compile(ValidationCallBack);
+
+                // we create an empty xml from the schema definition so we can walk the elements for each node to help us build the final output Xml
+                XMLDoc.LoadXml(GenerateEmptyXMLFromSchema(schema));
+
+                using (XMLWriter writer = new XMLWriter())
+                {
+
+                    writer.WriteCommentstring("CAHPS Hospice Survey XML File Specification Version 1.1");
+
+                    XmlNode node = XMLDoc.GetElementsByTagName("vendordata")[0];
+
+                    if (ds[0].DataTable.Rows.Count > 0)
+                    {
+                        WriteVendorSection(ds, writer,schema);
+                    }
+                    else throw new Exception("Vendor level result set is missing");
+
+
+                    XmlDocumentEx returnXMLdoc = new XmlDocumentEx();
+                    returnXMLdoc.Schemas.Add(schema);
+                    returnXMLdoc.LoadXml(writer.XmlString);
+
+                    returnXMLdoc.Validate();
+
+                    return returnXMLdoc;
+                }
+
+            }
+            else return null;
+        }
+
+        private void WriteVendorSection(List<ExportDataSet> dsList, XMLWriter writer, XmlSchema schema)
+        {
+            XmlNode node = XMLDoc.GetElementsByTagName("vendordata")[0];
+
+            writer.StartElement(node.Name);
+
+
+            ExportDataSet vendorDataSet = dsList[0];
+            
+            foreach (XmlNode childnode in node.ChildNodes)
+            {
+                CreateElementString(vendorDataSet.DataTable.Rows[0], string.Format("{0}.{1}", node.Name, childnode.Name), childnode.Name, writer);
+            }
+
+            if (dsList[1].DataTable.Rows.Count > 0)
+            {
+                WriteHopsiceSection(dsList, writer);
+            }
+            else throw new Exception("Hospice level result set is missing");
+
+            if (dsList[2].DataTable.Rows.Count > 0)
+            {
+                WriteDecedentLevelSection(dsList, writer);
+            }
+            else throw new Exception("Decendent level result set is missing");
+
+            
+
+            writer.EndElement();
+        }
+
+        private void WriteHopsiceSection(List<ExportDataSet> dsList, XMLWriter writer)
+        {
+
+            XmlNode node = XMLDoc.GetElementsByTagName("hospicedata")[0];
+
+            // group by reference-month, reference-yr, and provider-id
+            var distinctRows = dsList[1].DataTable.AsEnumerable().GroupBy(x => new { referenceMonth = x.Field<string>("hospicedata.reference-month"), referenceYear = x.Field<string>("hospicedata.reference-yr"), providerId = x.Field<string>("hospicedata.provider-id") }).Select(grp => grp.First());
+  
+            foreach (object dr in distinctRows)
+            {
+                writer.StartElement(node.Name);
+
+                foreach (XmlNode childnode in node.ChildNodes)
+                {
+                    CreateElementString((DataRow)dr, string.Format("{0}.{1}", node.Name, childnode.Name), childnode.Name, writer);
+                }
+
+                writer.EndElement();
+
+            }
+        }
+
+        private void WriteDecedentLevelSection(List<ExportDataSet> dsList, XMLWriter writer)
+        {
+            XmlNode node = XMLDoc.GetElementsByTagName("decedentleveldata")[0];
+
+            foreach (DataRow dr in dsList[2].DataTable.Rows)
+            {
+                writer.StartElement(node.Name);
+
+                foreach (XmlNode childnode in node.ChildNodes)
+                {
+                    CreateElementString(dr, string.Format("{0}.{1}", node.Name, childnode.Name), childnode.Name, writer);
+                }
+
+                string provider_id = dr["decedentleveldata.provider-id"].ToString();
+                string decedent_id = dr["decedentleveldata.decedent-id"].ToString();
+
+                string searchExpression = string.Format("[caregiverresponse.provider-id]={0} and [caregiverresponse.decedent-id]={1}", provider_id, decedent_id);
+                DataRow prRow = dsList[3].DataTable.Select(searchExpression)[0];
+
+                if (!AreAllColumnsEmpty(prRow, new int[]{0,1}))
+                {
+                    WriteCareGiverResponseSection(prRow, writer, node.LastChild);
+                }
+
+
+                writer.EndElement();
+
+            }
+        }
+
+        private void WriteCareGiverResponseSection(DataRow dr, XMLWriter writer, XmlNode node)
+        {
+            writer.StartElement(node.Name);
+
+            foreach (XmlNode childnode in node.ChildNodes)
+            {
+                CreateElementString(dr, string.Format("{0}.{1}", node.Name, childnode.Name), childnode.Name, writer);
+            }
+
+            writer.EndElement();
+            
+        }
+    }
+}
