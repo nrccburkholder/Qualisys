0b2932b42026d68f55967ef422f64c795c3286ed
ccaouette <ccaouette@nationalresearch.com>
2015-08-14 08:25:32 -0500

    CHG0032071

--- Databases/NRC_Datamart_ETL/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/csp_GetSUFacilityExtractData.proc.sql
+++ Databases/NRC_Datamart_ETL/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/csp_GetSUFacilityExtractData.proc.sql
@@ -1,12 +1,21 @@
 ﻿CREATE PROCEDURE [dbo].[csp_GetSUFacilityExtractData] 
 	@ExtractFileID int
 AS
+BEGIN
 	SET NOCOUNT ON 
+
+
+	DECLARE @oExtractRunLogID INT;
+	DECLARE @currDateTime1 DATETIME = GETDATE();
+	DECLARE @currDateTime2 DATETIME;
+	DECLARE @TaskName VARCHAR(200) =  OBJECT_NAME(@@PROCID);
+	EXEC [InsertExtractRunLog] @ExtractFileID, @TaskName, @currDateTime1, @ExtractRunLogID = @oExtractRunLogID OUTPUT;
 	
 	declare @EntityTypeID int
 	set @EntityTypeID = 6 -- SampleUnit
 
 	select convert(varchar,su.SAMPLEUNIT_ID) as sampleunitid,
+		   facilityAttrs.SUFacility_ID as Facility_ID,
 		   facilityAttrs.strFacility_nm as name,
 		   facilityAttrs.MedicareNumber as MedicareNumber,
 		   facilityAttrs.AHA_id as AHAIdent,
@@ -36,5 +45,13 @@ AS
 						and IsDeleted = 0 ) eh  on su.SAMPLEUNIT_ID = eh.PKey1
 	 
 	for xml auto
+
+	SET @currDateTime2 = GETDATE();
+	SELECT @oExtractRunLogID,@currDateTime2,@TaskName
+	EXEC [UpdateExtractRunLog] @oExtractRunLogID, @currDateTime2;
+
+END
+
+GO
 
 
