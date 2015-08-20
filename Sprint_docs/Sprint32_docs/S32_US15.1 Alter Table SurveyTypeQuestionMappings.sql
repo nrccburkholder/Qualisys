1ad664275274af4e448d5cff92c924fb8b406d7e
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2015-08-20 09:56:26 -0500

    S32 US15 join in dbo.SubType, cuz IDs are different between environments

--- Sprint_docs/Sprint32_docs/S32_US15.1 Alter Table SurveyTypeQuestionMappings.sql
+++ Sprint_docs/Sprint32_docs/S32_US15.1 Alter Table SurveyTypeQuestionMappings.sql
@@ -18,23 +18,28 @@ if not exists (select * from sys.tables st inner join sys.columns sc on st.objec
 
 if not exists (select * from sys.tables st inner join sys.columns sc on st.object_id=sc.object_id where st.name='SurveyTypeQuestionMappings' and sc.name='isMeasure')
 	ALTER TABLE SurveyTypeQuestionMappings add isMeasure bit
-
+go
 update SurveyTypeQuestionMappings set isATA=0, isMeasure=0
 
-update SurveyTypeQuestionMappings 
-set isATA=1 
-where SurveyType_id=10
-and SubType_id=9
-and qstncore in (50175,50218,50222,50223,50224,50225,50229,50230,50234,50235,50236,50238,50240,50241,50243,50245,50247,50248,50249,50250,50251,50252,50253,50255,50256,50699,50700,50725,51426)
+update stqm
+set isATA=1
+from SurveyTypeQuestionMappings stqm 
+inner join subtype st on stqm.subtype_id=st.subtype_id
+where stqm.SurveyType_id=10
+and st.subtype_nm='ACO-8'
+and stqm.qstncore in (50175,50218,50222,50223,50224,50225,50229,50230,50234,50235,50236,50238,50240,50241,50243,50245,50247,50248,50249,50250,50251,50252,50253,50255,50256,50699,50700,50725,51426)
 
-update SurveyTypeQuestionMappings 
+update stqm
 set isATA=1 
-where SurveyType_id=10
-and SubType_id=10
-and qstncore in (50175,50218,50222,50223,50224,50225,50226,50229,50230,50234,50235,50236,50238,50240,50241,50243,50245,50247,50248,50249,50250,50251,50252,50253,50255,50256,50699,50700,50725,51426)
+from SurveyTypeQuestionMappings stqm 
+inner join subtype st on stqm.subtype_id=st.subtype_id
+where stqm.SurveyType_id=10
+and st.subtype_nm='ACO-12'
+and stqm.qstncore in (50175,50218,50222,50223,50224,50225,50226,50229,50230,50234,50235,50236,50238,50240,50241,50243,50245,50247,50248,50249,50250,50251,50252,50253,50255,50256,50699,50700,50725,51426)
 
-update SurveyTypeQuestionMappings 
+update stqm
 set isMeasure=1 
-where SurveyType_id=10
-and qstncore in (50180,50182,50184,50186,50189,50190,50191,50193,50194,50196,50197,50201,50202,50203,50210,50211,50212,50213,50214,50215,50220,50221,50222,50223,50224,50225,50229,50230,50234,50235,50237,50239,50240,50249,50250,50251,50252)
+from SurveyTypeQuestionMappings stqm 
+where stqm.SurveyType_id=10
+and stqm.qstncore in (50180,50182,50184,50186,50189,50190,50191,50193,50194,50196,50197,50201,50202,50203,50210,50211,50212,50213,50214,50215,50220,50221,50222,50223,50224,50225,50229,50230,50234,50235,50237,50239,50240,50249,50250,50251,50252)
 
