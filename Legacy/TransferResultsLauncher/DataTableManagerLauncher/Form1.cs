88a817399eedae1b3365345e88fa5ca7e352d158
Brendan Goble <bgoble@nationalresearch.com>
2015-10-26 16:04:36 -0500

    Created data table manager launcher.

---
+++ Legacy/TransferResultsLauncher/DataTableManagerLauncher/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace DataTableManagerLauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Process.Start("DataTableMgr.exe");
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
