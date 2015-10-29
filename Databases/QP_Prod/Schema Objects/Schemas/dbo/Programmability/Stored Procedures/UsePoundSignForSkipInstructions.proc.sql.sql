afb9307091998a2b9ebb4b22ca6112723f933d43
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2015-10-29 14:30:59 -0500

    CHG0032179 - Atlas Team Release of Sprints 32 and 35

--- Databases/QP_Prod/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UsePoundSignForSkipInstructions.proc.sql.sql
+++ Databases/QP_Prod/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/UsePoundSignForSkipInstructions.proc.sql.sql
@@ -1,8 +1,30 @@
 create procedure dbo.UsePoundSignForSkipInstructions
-@survey_id int
+@survey_id int,
+@lang_id int
 as
-select 1 
-from SurveyType st
+
+declare @SurveyType varchar(25)
+declare @Language varchar(25)
+declare @SkipGoPhrase varchar(50)
+declare @SkipEndPhrase varchar(50)
+declare @FormatOverride varchar(255)
+declare @bitUsePoundForSkipInstructions bit
+declare @bitSkipRepeatsScaleText bit
+
+select @bitUsePoundForSkipInstructions = st.UsePoundSignForSkipInstructions, 
+	@bitSkipRepeatsScaleText = st.SkipRepeatsScaleText,
+	@SurveyType = Surveytype_dsc from SurveyType st
 inner join Survey_def sd on st.SurveyType_id = sd.SurveyType_id 
 where survey_id=@survey_id
-and st.UsePoundSignForSkipInstructions=1
+
+select @Language = Language, @SkipGoPhrase = SkipGoPhrase, @SkipEndPhrase = SkipEndPhrase from Languages 
+where LangId = @lang_id
+
+select @FormatOverride = strparam_value from QualPro_Params where
+strparam_nm = 'SkipInstructionFormat - ' + @SurveyType + ' + ' + @Language
+
+select @bitUsePoundForSkipInstructions UsePoundSignForSkipInstructions, 
+	IsNull(@FormatOverride, '') FormatOverride,
+	@bitSkipRepeatsScaleText SkipRepeatsScaleText,
+	IsNull(@SkipGoPhrase, '') SkipGoPhrase,
+	IsNull(@SkipEndPhrase, '') SkipEndPhrase
