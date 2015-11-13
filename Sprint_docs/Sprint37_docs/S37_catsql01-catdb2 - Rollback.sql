b8118b68cf6b65f534c3c6c359cbb0e96009f51c
Chris Burkholder <cburkholder@nationalresearch.com>
2015-11-13 10:58:16 -0600

    Added headers, updated zip.

---
+++ Sprint_docs/Sprint37_docs/S37_catsql01-catdb2 - Rollback.sql
@@ -0,0 +1,11 @@
+/*
+
+Sprint 37 SQLCMD Script for Hulk/Longhorn/Medusa
+
+\GIT2015\Sprint_docs\Sprint37_docs\S37_catsql01-catdb2 - Rollback.sql
+
+Chris Burkholder
+
+*/
+
+:r "\GIT2015\Sprint_docs\Sprint37_docs\S37_US8 CEM Hospice XML v 2.0 ROLLBACK.sql"
--- Sprint_docs/Sprint37_docs/S37_catsql01-catdb2.sql
+++ Sprint_docs/Sprint37_docs/S37_catsql01-catdb2.sql
@@ -11,3 +11,5 @@ Chris Burkholder
 :r "\GIT2015\Sprint_docs\Sprint37_docs\S37_US7.1 Add Missing Caregiver disposition_NRC_Datamart.sql"
 
 :r "\GIT2015\Sprint_docs\Sprint37_docs\S37_US8.2 CEM Hospice ExportPostProcess00000008.sql"
+
+:r "\GIT2015\Sprint_docs\Sprint37_docs\S37_US8 CEM Hospice XML v 2.0.sql"
