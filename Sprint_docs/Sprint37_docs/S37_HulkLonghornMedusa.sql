d010e9570c217930546cfaab5bbd9db391d666f7
Chris Burkholder <cburkholder@nationalresearch.com>
2015-11-13 10:37:23 -0600

    Added headers to deployment SQLCMD files

--- Sprint_docs/Sprint37_docs/S37_HulkLonghornMedusa.sql
+++ Sprint_docs/Sprint37_docs/S37_HulkLonghornMedusa.sql
@@ -1,2 +1,11 @@
+/*
+
+Sprint 37 SQLCMD Script for Hulk/Longhorn/Medusa
+
+\GIT2015\Sprint_docs\Sprint37_docs\S37_HulkLonghornMedusa.sql
+
+Chris Burkholder
+
+*/
 
 :r "\GIT2015\Sprint_docs\Sprint37_docs\S37_US10.1 Add ICD10 to HHCAHPS Export Files.sql"
--- Sprint_docs/Sprint37_docs/S37_catsql01-catdb2.sql
+++ Sprint_docs/Sprint37_docs/S37_catsql01-catdb2.sql
@@ -1,3 +1,12 @@
+/*
+
+Sprint 37 SQLCMD Script for Hulk/Longhorn/Medusa
+
+\GIT2015\Sprint_docs\Sprint37_docs\S37_catsql01-catdb2.sql
+
+Chris Burkholder
+
+*/
 
 :r "\GIT2015\Sprint_docs\Sprint37_docs\S37_US7.1 Add Missing Caregiver disposition_NRC_Datamart.sql"
 
