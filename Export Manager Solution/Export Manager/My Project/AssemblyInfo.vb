de6a99bf65157d0fe620df2bde8025827d7049b8
Brendan Goble <bgoble@nationalresearch.com>
2015-11-17 08:11:40 -0600

    Fix: updated export service version

--- Export Manager Solution/Export Manager/My Project/AssemblyInfo.vb
+++ Export Manager Solution/Export Manager/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.6.15.0")>
-<Assembly: AssemblyFileVersion("4.6.15.0")>
+<Assembly: AssemblyVersion("4.6.16.0")>
+<Assembly: AssemblyFileVersion("4.6.16.0")>
