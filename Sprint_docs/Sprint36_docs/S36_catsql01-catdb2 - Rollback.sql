953712b6ef57dd91bc49134fbc58e35d4653e268
Chris Burkholder <cburkholder@nationalresearch.com>
2015-11-18 11:14:17 -0600

    Fix: removed scripts already deployed for Hospice

--- Sprint_docs/Sprint36_docs/S36_catsql01-catdb2 - Rollback.sql
+++ Sprint_docs/Sprint36_docs/S36_catsql01-catdb2 - Rollback.sql
@@ -10,6 +10,6 @@ Chris Burkholder
 
 :r "\ATLASRelease36&37\Sprint36_docs\S36_US17.1 Add Hospice Disavowal disposition to NRC_Datamart ROLLBACK.sql"
 
-:r "\ATLASRelease36&37\Sprint36_docs\S36_US12 CEM Hospice XML v 1.3_rollback.sql"
+-- already deployed :r "\ATLASRelease36&37\Sprint36_docs\S36_US12 CEM Hospice XML v 1.3_rollback.sql"
 
--- already deployed ?? :r "\ATLASRelease36&37\Sprint36_docs\S36_US11 CAHPS Hospice exclude caregiverresponse section_rollback.sql"
+-- already deployed :r "\ATLASRelease36&37\Sprint36_docs\S36_US11 CAHPS Hospice exclude caregiverresponse section_rollback.sql"
--- Sprint_docs/Sprint36_docs/S36_catsql01-catdb2.sql
+++ Sprint_docs/Sprint36_docs/S36_catsql01-catdb2.sql
@@ -10,6 +10,6 @@ Chris Burkholder
 
 :r "\ATLASRelease36&37\Sprint36_docs\S36_US17.1 Add Hospice Disavowal disposition to NRC_Datamart.sql"
 
-:r "\ATLASRelease36&37\Sprint36_docs\S36_US12 CEM Hospice XML v 1.3.sql"
+-- already deployed :r "\ATLASRelease36&37\Sprint36_docs\S36_US12 CEM Hospice XML v 1.3.sql"
 
--- already deployed ?? :r "\ATLASRelease36&37\Sprint36_docs\S36_US11 CAHPS Hospice exclude caregiverresponse section.sql"
+-- already deployed :r "\ATLASRelease36&37\Sprint36_docs\S36_US11 CAHPS Hospice exclude caregiverresponse section.sql"
