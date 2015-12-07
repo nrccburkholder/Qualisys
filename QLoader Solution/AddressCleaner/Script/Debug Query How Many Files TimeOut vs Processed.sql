7fa0c91bf4165cd963acf539484a7b73c2f0100d
Christian Amelinckx <camelinckx@nationalresearch.com>
2015-12-07 14:11:50 -0600

    Fix: Enhanced script to see trend across days

--- QLoader Solution/AddressCleaner/Script/Debug Query How Many Files TimeOut vs Processed.sql
+++ QLoader Solution/AddressCleaner/Script/Debug Query How Many Files TimeOut vs Processed.sql
@@ -2,34 +2,39 @@ USE QP_Load
 GO
 
 DECLARE @FromDate DATETIME
-SET @FromDate = '2015-12-07'
+SET @FromDate = '2015-11-20'
 
 SELECT
-	COUNT(*) As Total, 'Loaded & Cleaned' AS Status
+	DATEADD(dd, 0, DATEDIFF(dd, 0, DFSH1.datOccurred)) AS Date, COUNT(*) As Total, 'Loaded & Cleaned' AS Status
 FROM
 	[dbo].[DataFileState_History] DFSH1
 	INNER JOIN
 	[dbo].[DataFileState_History] DFSH4 ON DFSH1.DataFile_id = DFSH4.DataFile_id AND DFSH4.State_Id = 4
 WHERE
-DFSH1.datOccurred > @FromDate AND DFSH1.State_Id = 1
+	DFSH1.datOccurred > @FromDate AND DFSH1.State_Id = 1
+GROUP BY
+	DATEADD(dd, 0, DATEDIFF(dd, 0, DFSH1.datOccurred))
 
 UNION 
 
 SELECT
-	COUNT(*) As Total, 'Timed Out' As Status
+	DATEADD(dd, 0, DATEDIFF(dd, 0, DFSH1.datOccurred)) AS Date, COUNT(*) As Total, 'Timed out' As Status
 FROM
 	[dbo].[DataFileState_History] DFSH1
 	INNER JOIN
 	[dbo].[DataFileState] DFSH11 ON DFSH1.DataFile_id = DFSH11.DataFile_id AND DFSH11.State_Id = 11 AND DFSH11.StateParameter = 'Address Cleaning Exception: The operation has timed out'
 WHERE
 	DFSH1.datOccurred > @FromDate
-AND DFSH1.State_Id = 1
+		AND 
+	DFSH1.State_Id = 1
+GROUP BY
+	DATEADD(dd, 0, DATEDIFF(dd, 0, DFSH1.datOccurred))
 
 
 UNION
 
 SELECT
-	COUNT(*) As Total, 'Abandoned for other reason' As Status
+	DATEADD(dd, 0, DATEDIFF(dd, 0, DFSH1.datOccurred)) AS Date, COUNT(*) As Total, 'Abandoned for other reason' As Status
 FROM
 	[dbo].[DataFileState_History] DFSH1
 	INNER JOIN
@@ -37,4 +42,10 @@ FROM
 	AND DFSH11.StateParameter <> 'Address Cleaning Exception: The operation has timed out'
 WHERE
 	DFSH1.datOccurred > @FromDate
-AND DFSH1.State_Id = 1
+		AND
+	DFSH1.State_Id = 1
+GROUP BY
+	DATEADD(dd, 0, DATEDIFF(dd, 0, DFSH1.datOccurred))
+
+ORDER BY
+	1 DESC,3
