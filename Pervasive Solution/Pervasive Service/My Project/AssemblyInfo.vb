3cff9295daccc9349f8658295e4a1ad3511bbacd
Brendan Goble <bgoble@nationalresearch.com>
2015-12-14 10:54:10 -0600

    Added nuspec to pervasive service

--- Pervasive Solution/Pervasive Service/My Project/AssemblyInfo.vb
+++ Pervasive Solution/Pervasive Service/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("ea7a147d-72bc-43c5-bbfd-b0c22ab4b2ef")> 
+<Assembly: Guid("ea7a147d-72bc-43c5-bbfd-b0c22ab4b2ef")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.1.2")> 
-<Assembly: AssemblyFileVersion("1.0.1.2")> 
+<Assembly: AssemblyVersion("1.0.2.0")>
+<Assembly: AssemblyFileVersion("1.0.2.0")>
