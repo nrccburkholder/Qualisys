a01188e34715b21b0ca8cd9d622d5d44e464f507
tbutler <tbutler@nationalresearch.com>
2015-12-23 09:24:35 -0600

    Update ATLASRelease38&39.zip to include S39_US19 SV_SamplePlan_Has_Consistent_CCNs.sql

--- Sprint_docs/Sprint39_docs/S39_PrimeGatorNRC10 - rollback.sql
+++ Sprint_docs/Sprint39_docs/S39_PrimeGatorNRC10 - rollback.sql
@@ -16,5 +16,7 @@ Chris Burkholder
 
 :r "\ATLASRelease38&39\Sprint39_docs\S39_US11_HCAHPS_No_Custom_Methodologies_ROLLBACK.sql"
 
+:r "\ATLASRelease38&39\Sprint39_docs\S39_US19 SV_SamplePlan_Has_Consistent_CCNs_ROLLBACK.sql"
+
 -- this was part of an off-cycle release
 --:r "\ATLASRelease38&39\Sprint39_docs\S39_US17.2_ Hospice_CAHPS_2.0_Language-Speak Question_ROLLBACK.sql"
