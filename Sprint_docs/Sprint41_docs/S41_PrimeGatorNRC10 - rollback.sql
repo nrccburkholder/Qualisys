1de1d857f745aaa0ba485af2b6750d9ae04112d0
Chris Burkholder <cburkholder@nationalresearch.com>
2016-02-05 14:46:48 -0600

    New: DB deploy Scripts for S41 and S42 so far

---
+++ Sprint_docs/Sprint41_docs/S41_PrimeGatorNRC10 - rollback.sql
@@ -0,0 +1,13 @@
+/*
+
+Sprint 41 SQLCMD Script for Prime/Gator/NRC10
+
+\Sprint_docs\Sprint41_docs\S41_PrimeGatorNRC10 - rollback.sql
+
+Chris Burkholder
+
+*/
+
+:r "\ATLASRelease41&42\Sprint41_docs\S41 US20 T1 OAS CAHPS Skip Instructions - ROLLBACK.sql"
+
+:r "\ATLASRelease41&42\Sprint41_docs\S41_US21 OAS Keep Most Complete Return ROLLBACK.sql"
---
+++ Sprint_docs/Sprint42_docs/S42_catsql01-catdb2 - ROLLBACK.sql
@@ -0,0 +1,12 @@
+/*
+
+Sprint 42 SQLCMD Script for catsql01-catdb2
+
+\Sprint_docs\Sprint42_docs\S42_catsql01-catdb2.sql
+
+ROLLBACK
+
+*/
+
+:r "\ATLASRelease41&42\Sprint42_docs\S42 US9 T3 OAS Dispo processing CATDB2 - Rollback.sql"
+
