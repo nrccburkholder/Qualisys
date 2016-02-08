96d41c690bc4db22b972aed01bc16cd9be5eb0f7
tbutler <tbutler@nationalresearch.com>
2016-02-08 16:32:00 -0600

    Update: S42_catsql01-catdb2 and S42_PrimeGatorNRC10 added scripts for stories 12 & 13

--- Sprint_docs/Sprint42_docs/S42_PrimeGatorNRC10 - rollback.sql
+++ Sprint_docs/Sprint42_docs/S42_PrimeGatorNRC10 - rollback.sql
@@ -10,3 +10,4 @@ Chris Burkholder
 
 :r "\ATLASRelease41&42\Sprint42_docs\S42 US9 T2 OAS Dispo processing QP_PROD - Rollback.sql"
 
+:r "\ATLASRelease41&42\Sprint42_docs\S42 US12_13  OAS ETL Updates NRC10 ROLLBACK.sql"
--- Sprint_docs/Sprint42_docs/S42_PrimeGatorNRC10.sql
+++ Sprint_docs/Sprint42_docs/S42_PrimeGatorNRC10.sql
@@ -9,3 +9,5 @@ Chris Burkholder
 */
 
 :r "\ATLASRelease41&42\Sprint42_docs\S42 US9 T2 OAS Dispo processing QP_PROD.sql"
+
+:r "\ATLASRelease41&42\Sprint42_docs\S42 US12_13  OAS ETL Updates NRC10.sql"
