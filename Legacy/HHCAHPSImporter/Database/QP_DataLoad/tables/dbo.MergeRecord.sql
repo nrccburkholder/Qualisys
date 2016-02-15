039e80bf6123b231aacd2ee37579ec113ea378df
Brendan Goble <bgoble@nationalresearch.com>
2016-02-15 13:23:27 -0600

    New: cleaned up validations sproc based on code review

--- Legacy/HHCAHPSImporter/Database/QP_DataLoad/tables/dbo.MergeRecord.sql
+++ Legacy/HHCAHPSImporter/Database/QP_DataLoad/tables/dbo.MergeRecord.sql
@@ -2,8 +2,8 @@ create table dbo.MergeRecord (
 	SampleYear int not null,
 	SampleMonth int not null,
 	CCN varchar(10) not null,
-	MatchKey varchar(100) not null,
-	RecordXml varchar(max),
+	MatchKey nvarchar(100) not null,
+	RecordXml nvarchar(max),
 
 	constraint PK_MergeRecord primary key clustered (SampleYear, SampleMonth, CCN, MatchKey)
 )
