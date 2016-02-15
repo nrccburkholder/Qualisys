37c133af110eee4de78938a4b6a823eac8a32704
Chris Burkholder <cburkholder@nationalresearch.com>
2016-02-15 14:52:53 -0600

    Updated: added broken out Sampling SP to deploy

--- Sprint_docs/Sprint42_docs/S42_PrimeGatorNRC10 - rollback.sql
+++ Sprint_docs/Sprint42_docs/S42_PrimeGatorNRC10 - rollback.sql
@@ -11,3 +11,5 @@ Chris Burkholder
 :r "\ATLASRelease41&42\Sprint42_docs\S42 US9 T2 OAS Dispo processing QP_PROD - Rollback.sql"
 
 :r "\ATLASRelease41&42\Sprint42_docs\S42 US12_13  OAS ETL Updates NRC10 ROLLBACK.sql"
+
+:r "\ATLASRelease41&42\Sprint42_docs\S42_US12  ALTER PROCEDURE QCL_SelectEncounterUnitEligibility ROLLBACK.sql"
