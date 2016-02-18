20a34564b7d0d85bbed14d10ff1303b30e47fb75
Chris Burkholder <cburkholder@nationalresearch.com>
2016-02-18 14:10:20 -0600

    New: Signing files for NRCAuth Admin and QSI

--- QualiSys Scanner Interface Solution/QualiSys Scanner Interface/My Project/AssemblyInfo.vb
+++ QualiSys Scanner Interface Solution/QualiSys Scanner Interface/My Project/AssemblyInfo.vb
@@ -12,7 +12,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("National Research Corporation")> 
 <Assembly: AssemblyProduct("QualiSys Scanner Interface")> 
-<Assembly: AssemblyCopyright("Copyright © 2015")> 
+<Assembly: AssemblyCopyright("Copyright © 2016")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.12.0.0")>
-<Assembly: AssemblyFileVersion("1.12.0.0")>
+<Assembly: AssemblyVersion("1.12.1.0")> 
+<Assembly: AssemblyFileVersion("1.12.1.0")> 
