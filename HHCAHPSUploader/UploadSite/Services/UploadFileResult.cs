203bd284fece6b18d4d9517d3a1b80758d40981f
Brendan Goble <bgoble@nationalresearch.com>
2016-02-23 13:45:46 -0600

    New: switched uploader from mvc 6 to 5

---
+++ HHCAHPSUploader/UploadSite/Services/UploadFileResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace UploadSite.Services
+{
+    public class UploadFileResult
+    {
+        public bool Success => string.IsNullOrEmpty(Error);
+        public bool IsZip => !string.IsNullOrEmpty(ZipName);
+        public string ZipName { get; set; }
+        public string FileName { get; set; }
+        public string OriginalName { get; set; }
+        public string FinalName { get; set; }
+        public string Error { get; set; }
+        public HttpPostedFileBase UploadData { get; set; }
+        public byte[] ZipData { get; set; }
+        public bool IsZeroLength => IsZip ? ZipData.Length == 0 : UploadData.ContentLength == 0;
+    }
+}
