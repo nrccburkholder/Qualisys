72b6dc40107a962911c44468c93e24f2427f3214
Brendan Goble <bgoble@nationalresearch.com>
2016-02-24 15:51:54 -0600

    New: forcing https

--- HHCAHPSUploader/UploadSite/App_Start/FilterConfig.cs
+++ HHCAHPSUploader/UploadSite/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace UploadSite
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
