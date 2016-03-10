6f069c5837223bb43b000c73d5a5e9968fcfc6d9
tbutler <tbutler@nationalresearch.com>
2016-03-10 09:06:42 -0600

    New: S43 deployment scripts

---
+++ Sprint_docs/Sprint43_docs/S43_catsql01-catdb2 - ROLLBACK.sql
@@ -0,0 +1,12 @@
+/*
+
+Sprint 43 SQLCMD Script for catsql01-catdb2
+
+\Sprint_docs\Sprint43_docs\S43_catsql01-catdb2.sql
+
+*/
+
+
+:r "\ATLASRelease43&44\Sprint43_docs\S43_US13.2_ICH_Methodologies CATDB2 ROLLBACK.sql
+
+
