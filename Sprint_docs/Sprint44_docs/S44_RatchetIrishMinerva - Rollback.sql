47142d3d6d532ecf1937d6b125b4b0cabdd8f9d9
tbutler <tbutler@nationalresearch.com>
2016-03-16 08:07:39 -0500

    New:  S44_RatchetIrishMinerva scripts added

---
+++ Sprint_docs/Sprint44_docs/S44_RatchetIrishMinerva - Rollback.sql
@@ -0,0 +1,16 @@
+/*
+
+Sprint 44 SQLCMD Script for Ratchet/Irish/Minerva
+
+\Sprint_docs\Sprint44_docs\S44_RatchetIrishMinerva - Rollback.sql
+
+Chris Burkholder
+
+*/
+
+:r "\ATLASRelease43&44\Sprint44_docs\S44_11.1_CGCAHPSDispositions MINERVA ROLLBACK.sql"  
+
+:r "\ATLASRelease43&44\Sprint44_docs\S44_US14 Update Practice Site Submission File Proc - QP_Comments - ROLLBACK.sql"
+
+:r "\ATLASRelease43&44\Sprint44_docs\S45_US15 CG Update Patient Submission Procs ROLLBACK.sql"
+
---
+++ Sprint_docs/Sprint44_docs/S44_RatchetIrishMinerva.sql
@@ -0,0 +1,18 @@
+/*
+
+Sprint 44 SQLCMD Script for Ratchet/Irish/Minerva
+
+\Sprint_docs\Sprint44_docs\S44_RatchetIrishMinerva.sql
+
+Chris Burkholder
+
+*/
+
+:r "\ATLASRelease43&44\Sprint44_docs\S44_11.1_CGCAHPSDispositions MINERVA.sql"
+
+:r "\ATLASRelease43&44\Sprint44_docs\S44_US14 Update Practice Site Submission File Proc - QP_Comments.sql"
+
+:r "\ATLASRelease43&44\Sprint44_docs\S45_US15 CG Update Patient Submission Procs.sql"
+
+:r "\ATLASRelease43&44\Sprint44_docs\S44_US13 Update Group Submission File Proc.sql"
+
