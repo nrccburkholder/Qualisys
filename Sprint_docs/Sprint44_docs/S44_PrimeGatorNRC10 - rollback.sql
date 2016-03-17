bf509a1f9e4511bef0008224ae2c7e9eb8abc3f0
tbutler <tbutler@nationalresearch.com>
2016-03-17 09:03:23 -0500

    Update:  S44_PrimeGatorNRC10 -  include new script for adding Sampling column to PracticeSite table.

--- Sprint_docs/Sprint44_docs/S44_PrimeGatorNRC10 - rollback.sql
+++ Sprint_docs/Sprint44_docs/S44_PrimeGatorNRC10 - rollback.sql
@@ -10,4 +10,6 @@ Chris Burkholder
 
 :r "\ATLASRelease43&44\Sprint44_docs\S44_11.1_CGCAHPSDispositions NRC10 ROLLBACK.sql"
 
-:r "\ATLASRelease43&44\Sprint44_docs\S44_12 CGCAHPSCompleteness NRC10 ROLLBACK.sql
+:r "\ATLASRelease43&44\Sprint44_docs\S44_12 CGCAHPSCompleteness NRC10 ROLLBACK.sql"
+
+:r "\ATLASRelease43&44\Sprint44_docs\S44_US14 Update Practice Site Submission File Proc - QP_Prod - ROLLBACK.sql"
