44fa0c4795ee71afbd3e3d5a8d0a6c5583c3a1f9
Chris Burkholder <cburkholder@nationalresearch.com>
2016-04-14 17:24:18 -0500

    Fix: Personalization Code 31 disassociated

---
+++ Sprint_docs/Sprint47_docs/S47 ATL-226 Personalization Code 31 Analysis - Rollback.sql
@@ -0,0 +1,31 @@
+/*
+S47 ATL-226 Personalization Code 31 Analysis - Rollback.sql
+
+As Atlas team member, we would like analyze work to best configure personalization code 31 
+to not require any address field mappings, so that we can use it on handouts.
+
+Chris Burkholder
+
+select * from codes c, codestext ct, codetexttag ctt, tag t
+where c.code = ct.code
+and ct.codetext_id = ctt.codetext_id
+and ctt.tag_id = t.tag_id
+and c.code = 31
+*/
+
+--DO
+
+USE [QP_Prod]
+GO
+
+--delete from codetexttag where codetext_id = 48
+
+--ROLLBACK
+
+insert into CODETEXTTAG (CODETEXT_ID,TAG_ID,INTSTARTPOS,INTLENGTH)
+values (48,23,215,11)
+insert into CODETEXTTAG (CODETEXT_ID,TAG_ID,INTSTARTPOS,INTLENGTH)
+values (48,28,2,9)
+insert into CODETEXTTAG (CODETEXT_ID,TAG_ID,INTSTARTPOS,INTLENGTH)
+values (48,29,11,10)
+
