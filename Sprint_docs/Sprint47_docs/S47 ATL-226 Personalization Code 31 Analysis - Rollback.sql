bec7f4366518aa7034901e38186eefbf6ccc3728
Chris Burkholder <cburkholder@nationalresearch.com>
2016-04-15 13:51:31 -0500

    Fix: Rollback now idempotent (reinserts only once)

--- Sprint_docs/Sprint47_docs/S47 ATL-226 Personalization Code 31 Analysis - Rollback.sql
+++ Sprint_docs/Sprint47_docs/S47 ATL-226 Personalization Code 31 Analysis - Rollback.sql
@@ -22,10 +22,13 @@ GO
 
 --ROLLBACK
 
+if not exists (select * from codetexttag where codetext_id = 48 and tag_id = 23)
 insert into CODETEXTTAG (CODETEXT_ID,TAG_ID,INTSTARTPOS,INTLENGTH)
 values (48,23,215,11)
+if not exists (select * from codetexttag where codetext_id = 48 and tag_id = 28)
 insert into CODETEXTTAG (CODETEXT_ID,TAG_ID,INTSTARTPOS,INTLENGTH)
 values (48,28,2,9)
+if not exists (select * from codetexttag where codetext_id = 48 and tag_id = 29)
 insert into CODETEXTTAG (CODETEXT_ID,TAG_ID,INTSTARTPOS,INTLENGTH)
 values (48,29,11,10)
 
