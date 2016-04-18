0c4d2d9e618afbb20e761f089f8500d465417e17
Chris Burkholder <cburkholder@nationalresearch.com>
2016-04-18 14:55:26 -0500

    Fix: Retry on CodeTextTag Analysis/test approach

--- Sprint_docs/Sprint47_docs/S47 ATL-226 Personalization Code 31 Analysis.sql
+++ Sprint_docs/Sprint47_docs/S47 ATL-226 Personalization Code 31 Analysis.sql
@@ -18,14 +18,17 @@ and c.code = 31
 USE [QP_Prod]
 GO
 
-delete from codetexttag where codetext_id = 48
+delete from codetexttag where codetext_id = 48 and tag_id in (28,29)
 
 --ROLLBACK
 /*
+if not exists (select * from codetexttag where codetext_id = 48 and tag_id = 23)
 insert into CODETEXTTAG (CODETEXT_ID,TAG_ID,INTSTARTPOS,INTLENGTH)
 values (48,23,215,11)
+if not exists (select * from codetexttag where codetext_id = 48 and tag_id = 28)
 insert into CODETEXTTAG (CODETEXT_ID,TAG_ID,INTSTARTPOS,INTLENGTH)
 values (48,28,2,9)
+if not exists (select * from codetexttag where codetext_id = 48 and tag_id = 29)
 insert into CODETEXTTAG (CODETEXT_ID,TAG_ID,INTSTARTPOS,INTLENGTH)
 values (48,29,11,10)
 */
