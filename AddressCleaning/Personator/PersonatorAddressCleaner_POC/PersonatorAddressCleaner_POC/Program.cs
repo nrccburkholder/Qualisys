47a8dce72d52e145ef93c5e8fb90c3d3a940a1e6
Christian Amelinckx <camelinckx@nationalresearch.com>
2016-04-21 14:56:21 -0500

    New: calling Personator using JSON

--- AddressCleaning/Personator/PersonatorAddressCleaner_POC/PersonatorAddressCleaner_POC/Program.cs
+++ AddressCleaning/Personator/PersonatorAddressCleaner_POC/PersonatorAddressCleaner_POC/Program.cs
@@ -9,6 +9,8 @@ using System.Web;
 using System.Net;
 using System.IO;
 using System.Xml;
+using System.Data;
+using System.Data.SqlClient;
 
 
 
@@ -18,6 +20,7 @@ namespace PersonatorAddressCleaner_POC
     {
         static void Main(string[] args)
         {
+
 
             Console.WriteLine("Personator Address Cleaner:  POC ");
             Console.WriteLine();
@@ -34,93 +37,179 @@ namespace PersonatorAddressCleaner_POC
 
         static void Start()
         {
-            HttpWebRequest request;
-            HttpWebResponse response;
-            Stream responseReader;
-            XmlDocument xmlDoc;
-
-            Uri apiUri = new Uri("https://personator.melissadata.net/v3/WEB/ContactVerify/doContactVerify");
-            string customerID = "99869570";
-            string tAction = "Check";
-            string tCols = "";
-            string tCentricHint = "Address";
-            string tAppend = "CheckError";
-            string tFirstName = "Timothy";
-            string tLastName = "Butler";
-            string tMiddleInit = "S";
-            string tFullName = $"{tFirstName} {tMiddleInit} {tLastName}";
-            string tCompany = "";
-            string tAddress1 = "610 S Evergreen Drive";
-            string tAddress2 = "";
-            string tCity = "Seward";
-            string tState = "Nebraska";
-            string tPostalCode = "68444";
-            string tCountry = "USA";
-            string tLastLines = "";
-            string tFreeForm = "";
-            string tPhone = "4026416896";
-            string tEmail = "tbutler1@neb.rr.com";
-
-            string parameters = "?t=" + "Testing.";
-            parameters += "&id=" + customerID;
-            parameters += "&act=" + tAction;
-            parameters += "&cols="+ tCols;
-            parameters += "&opt=CentricHint:" + tCentricHint;
-            parameters += "Append:" + tAppend;
-            parameters += "&first=" + tFirstName;
-            parameters += "&last=" + tLastName;
-            parameters += "&full=" + tFullName;
-            parameters += "&comp="+ tCompany;
-            parameters += "&a1=" + tAddress1;
-            parameters += "&a2=" + tAddress2;
-            parameters += "&city=" + tCity;
-            parameters += "&state=" + tState;
-            parameters += "&postal=" + tPostalCode;
-            parameters += "&ctry=" + tCountry;
-            parameters += "&lastlines=" + tLastLines;
-            parameters += "&freeform=" + tFreeForm;
-            parameters += "&email=" + tEmail;
-            parameters += "&phone=" + tPhone;
-            parameters += "&reserved=";
-
-            //https://personator.melissadata.net/v3/WEB/ContactVerify/doContactVerify?t=Testing.&id=99869570&act=Check,&cols=&opt=CentricHint:AddressAppend:CheckError&first=&last=&full=Mr. Timothy S. Butler&comp=&a1=610 S Evergreen Drove&a2=&city=Seward&state=NE&postal=68433&ctry=USA&lastlines=&freeform=&email=tbutler1@neb.rr.com&phone=4026416741&reserved=
-
-
-            string tRequest = apiUri + parameters;
-
-            // create the web request
-            request = (HttpWebRequest)WebRequest.Create(tRequest);
-
-            // get response
-            response = (HttpWebResponse)request.GetResponse();
-
-            // get the response into a reader
-            responseReader = response.GetResponseStream();
-
-            XmlTextWriter outXML = new XmlTextWriter("response.xml", null);
-
-            xmlDoc = new XmlDocument();
-            xmlDoc.Load(responseReader);
-            xmlDoc.Save(outXML);
-            outXML.Close();
-
-            response.Close();
-            responseReader.Close();
-
+            string responseText = MelissaDataApiJsonCall();
             Console.WriteLine();
-            Console.WriteLine(xmlDoc.InnerText);
-            Console.WriteLine();
+            Console.WriteLine(responseText);
+            Console.WriteLine(); 
+        }
 
-            WriteOutput(xmlDoc);
+        public static string MelissaDataApiJsonCall()
+        {
+            string TransmissionReference = "";
+            string CustomerID = "99869570";
+            string Actions = "Check";
+            string Options = "";
+            string Columns = "";
+            
+            //string RecordID = "1";
+            //string CompanyName = "NRC";
+            //string FullName = "Timothy S Butler";
+            //string AddressLine1 = "610 S Evergreen Drive";
+            //string AddressLine2 = "";
+            //string Suite = "";
+            //string City = "Seward";
+            //string State = "Nebraska";
+            //string PostalCode = "68444";
+            //string Country = "USA";
+            //string PhoneNumber = "4026416896";
+            //string EmailAddress = "tbutler1@neb.rr.com";
+            //string FreeForm = "";
+
+
+            /*
+            string RecordID = "1";
+            string CompanyName = "";
+            string FullName = "Christian Amelinckx";
+            string AddressLine1 = "2450 Sewell St";
+            string AddressLine2 = "";
+            string Suite = "";
+            string City = "Lincoln";
+            string State = "Nebraska";
+            string PostalCode = "68502";
+            string Country = "USA";
+            string PhoneNumber = "";
+            string EmailAddress = "";
+            string FreeForm = "";
+
+            {"Records":[{"AddressExtras":" ","AddressKey":"68502403050","AddressLine1":"2450 Sewell St","AddressLine2":" ","City":"Lincoln","CompanyName":" ","EmailAddress":" ","MelissaAddressKey":" ","NameFull":"Christian Amelinckx","PhoneNumber":" ","PostalCode":"68502-4030","RecordExtras":" ","RecordID":"1","Reserved":" ","Results":"AS01,NS01,NS05","State":"NE"}],"TotalRecords":"1","TransmissionReference":" ","TransmissionResults":" ","Version":"4.0.10"}
+             */
+
+            string environment = "STAGE";
+            int DataFile_id = 427336;
+            int Study_id = 4329;
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://personator.melissadata.net/v3/WEB/ContactVerify/doContactVerify");
+            httpWebRequest.ContentType = "text/json";
+            httpWebRequest.Method = "POST";
+
+            var serializer = new Newtonsoft.Json.JsonSerializer();
+            using (var sw = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                using (var tw = new Newtonsoft.Json.JsonTextWriter(sw))
+                {
+                    var requestData = new
+                        {
+                            TransmissionReference = TransmissionReference,
+                            CustomerID = CustomerID,
+                            Actions = Actions,
+                            Options = Options,
+                            Columns = Columns,
+                            Records = BuildAddressRecords(environment, DataFile_id, Study_id)
+                        };
+
+                    serializer.Serialize(tw, requestData);
+                }
+            }
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var responseText = streamReader.ReadToEnd();
+                return responseText;
+            }
 
 
         }
 
-
-        static void WriteOutput(XmlDocument xmlDoc)
+        private static object[] BuildAddressRecords(string environment, int DataFile_Id, int Study_Id)
         {
-            //TODO:
+            List<object> addressCleanRecords = new List<object>();
 
+            //Pull records from DataFile
+            DataTable encounters = GetDataFileRecords(environment, DataFile_Id, 5);
+
+            foreach (DataRow e in encounters.Rows)
+            {
+                string RecordID = e["DataFile_id"].ToString() + "_" + e["DF_id"].ToString();
+                string CompanyName = "";
+                string FullName;
+                string Middle = e["Middle"].ToString().Trim();
+                if (string.IsNullOrWhiteSpace(Middle))
+                { FullName = string.Concat(e["FName"], " ", e["LName"]); }
+                else
+                { FullName = string.Concat(e["FName"], " ", Middle, " ", e["LName"]); }
+                string AddressLine1 = e["Addr"].ToString();
+                string AddressLine2 = e["Addr2"].ToString();
+                string Suite = "";
+                string City = e["City"].ToString();
+                string State = e["ST"].ToString();
+                string PostalCode = e["Zip5"].ToString();
+                string Country = "";
+                string PhoneNumber = string.Concat(e["AreaCode"], e["Phone"]);
+                string EmailAddress = "";
+                string FreeForm = "";
+
+                var o = new
+                {
+                    RecordID = RecordID,
+                    CompanyName = CompanyName,
+                    FullName = FullName,
+                    AddressLine1 = AddressLine1,
+                    AddressLine2 = AddressLine2,
+                    Suite = Suite,
+                    City = City,
+                    State = State,
+                    PostalCode = PostalCode,
+                    Country = Country,
+                    PhoneNumber = PhoneNumber,
+                    EmailAddress = EmailAddress,
+                    FreeForm = FreeForm,
+                };
+
+                addressCleanRecords.Add(o);
+            }
+
+
+            return addressCleanRecords.ToArray();
+        }
+
+        private static DataTable GetDataFileRecords(string environment, int datafile_id, int maxRecords)
+        {
+            string serverName = GetEnvironmentQPLoadServerName(environment);
+
+            string connStr = string.Format("Server={0};Database=QP_Load;Trusted_Connection=True;", serverName);
+
+            using(SqlConnection conn = new SqlConnection(connStr))
+            {
+                string limitExpression = maxRecords > 0 ? string.Format(" TOP {0} ", maxRecords) : "";
+                string sql = string.Format("SELECT {0} * FROM DataFile_{1}", limitExpression, datafile_id);
+
+                SqlDataAdapter sda = new SqlDataAdapter(sql, conn);
+                DataTable table = new DataTable();
+                try
+                {
+                    conn.Open();
+                    sda.Fill(table);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+                return table;
+            }
+
+        }
+
+        private static string GetEnvironmentQPLoadServerName(string environment)
+        {
+            string serverName;
+            if (environment.ToUpper() == "STAGE")
+            {
+                serverName = "Cyclone";
+                return serverName;
+            }
+            {
+                throw new Exception(string.Format("Don't know how to reach environment: [{0}]", environment));
+            }
         }
     }
 }
