74c9e296c2ec1f323ce982d50c2a8f6f11639233
tbutler <tbutler@nationalresearch.com>
2016-04-26 13:47:10 -0500

    Update:  ATLASRelease45&46.zip - added scripts for S46_US7.1

---
+++ Sprint_docs/Sprint46_docs/S46_catsql01-catdb2 - Rollback.sql
@@ -0,0 +1,12 @@
+/*
+
+Sprint 46 SQLCMD Script for catsql01-catdb2
+
+\Sprint_docs\Sprint46_docs\S46_catsql01-catdb2.sql
+
+*/
+
+
+:r "\ATLASRelease45&46\Sprint46_docs\S46_US7.1 etl_ProcessSamplePopulationDispositionLogRecords_ROLLBACK.sql"
+
+
