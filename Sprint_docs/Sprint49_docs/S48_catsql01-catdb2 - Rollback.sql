e884b0befe0ad9ac9395072808145cc695e967db
Chris Burkholder <cburkholder@nationalresearch.com>
2016-05-25 09:28:14 -0500

    Fix Needed: do not have a rollback script here
    Verify with Dave G if one is needed or not

--- Sprint_docs/Sprint49_docs/S48_catsql01-catdb2 - Rollback.sql
+++ Sprint_docs/Sprint49_docs/S48_catsql01-catdb2 - Rollback.sql
@@ -6,5 +6,5 @@ Sprint 48 SQLCMD Script for catsql01-catdb2
 
 */
 
---:r "\ATLASRelease47&48\Sprint49_docs\S49 ATL-395 Hospice Language-Speak Question_ROLLBACK.sql"
+--:r "\ATLASRelease47&48\Sprint49_docs\???.sql"
 
