89d044045a7bc1e3b7ce3c43d92a0c7daeb9c958
Chris Burkholder <cburkholder@nationalresearch.com>
2016-06-22 16:04:37 -0500

    New: Scripts from Sprint 50 to release with 51

---
+++ Sprint_docs/Sprint50_docs/S51fromS50_PrimeGatorNRC10 - Rollback.sql
@@ -0,0 +1,9 @@
+/*
+
+Sprint 50 SQLCMD Script for Prime/Gator/NRC10
+
+\Sprint_docs\Sprint50_docs\S51fromS50_PrimeGatorNRC10 - Rollback.sql
+
+*/
+
+:r "\ATLASRelease49&50\Sprint50_docs\S50 ATL-278 CA Web Survey Migration - Rollback.sql"
