e9315f41046ccbf398a04629fbe464d1345b82d4
Chris Burkholder <cburkholder@nationalresearch.com>
2016-07-05 16:21:22 -0500

    New: Deploy scripts for Sprint 52 + added to .zip

---
+++ Sprint_docs/Sprint52_docs/S52_PrimeGatorNRC10.sql
@@ -0,0 +1,10 @@
+/*
+
+Sprint 52 SQLCMD Script for Prime/Gator/NRC10
+
+\Sprint_docs\Sprint52_docs\S52_PrimeGatorNRC10.sql
+
+*/
+
+:r "\ATLASRelease51&52\Sprint52_docs\S52 ATL-530 HCAHPS Update File Disposition.sql"
+
