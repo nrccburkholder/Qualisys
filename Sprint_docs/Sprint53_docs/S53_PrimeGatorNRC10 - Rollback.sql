6c479123daddb1e086acce21b71567c7fdf879e9
tbutler <tbutler@nationalresearch.com>
2016-08-08 13:44:55 -0500

    Update S53_PrimeGatorNRC10.sql and S53_catsql01-catdb2.sql with practicesite ETL script changes

--- Sprint_docs/Sprint53_docs/S53_PrimeGatorNRC10 - Rollback.sql
+++ Sprint_docs/Sprint53_docs/S53_PrimeGatorNRC10 - Rollback.sql
@@ -6,4 +6,5 @@ Sprint 53 SQLCMD Script for Prime/Gator/NRC10
 
 */
 
---:r "\ATLASRelease53&54\Sprint53_docs\"
+:r "\ATLASRelease53&54\Sprint53_docs\S53 ATL-564 alter table dbo.practicesite add CCN column NRC10 ROLLBACK.sql"
+
---
+++ Sprint_docs/Sprint53_docs/S53_catsql01-catdb2 - Rollback.sql
@@ -0,0 +1,11 @@
+/*
+
+Sprint 53 SQLCMD Script for catsql01-catdb2
+
+\Sprint_docs\Sprint53_docs\S532_catsql01-catdb2 - ROLLBACK.sql
+
+*/
+
+:r "\ATLASRelease53&54\Sprint53_docs\S53 ATL-564 alter table dbo.practicesite add CCN column CATDB ROLLBACK.sql"
+
+
