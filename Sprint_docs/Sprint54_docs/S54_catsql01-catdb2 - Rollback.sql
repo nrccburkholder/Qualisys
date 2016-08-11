3fe50238407c93aac8ccdd39dd4973ea6852b121
tbutler <tbutler@nationalresearch.com>
2016-08-11 13:04:50 -0500

    Added sprint 54 deployment scripts

---
+++ Sprint_docs/Sprint54_docs/S54_catsql01-catdb2 - Rollback.sql
@@ -0,0 +1,11 @@
+/*
+
+Sprint 54 SQLCMD Script for catsql01-catdb2
+
+\Sprint_docs\Sprint54_docs\S54_catsql01-catdb2 - ROLLBACK.sql
+
+*/
+
+:r "\ATLASRelease53&54\Sprint54_docs\S54 ATL-663 ICH submission support_rollback.sql"
+
+--:r "\ATLASRelease53&54\Sprint54_docs\S54 ATL-650 Clean Up Blank First Dispos for ICH ROLLBACK.sql"  THERE IS NO ROLLBACK FOR THIS SCRIPT
---
+++ Sprint_docs/Sprint54_docs/S54_catsql01-catdb2.sql
@@ -0,0 +1,11 @@
+/*
+
+Sprint 54 SQLCMD Script for catsql01-catdb2
+
+\Sprint_docs\Sprint54_docs\S54_catsql01-catdb2.sql
+
+*/
+
+:r "\ATLASRelease53&54\Sprint54_docs\S54 ATL-663 ICH submission support.sql"
+
+:r "\ATLASRelease53&54\Sprint54_docs\S54 ATL-650 Clean Up Blank First Dispos for ICH.sql" 
