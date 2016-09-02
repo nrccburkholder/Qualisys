957bc72e57f78477190ab678c93f7edf563c4110
Chris Burkholder <cburkholder@nationalresearch.com>
2016-09-02 10:34:20 -0500

    Sampling Householding now in deploy scripts

---
+++ Sprint_docs/Sprint57_docs/S57_PrimeGatorNRC10 - Rollback.sql
@@ -0,0 +1,9 @@
+/*
+
+Sprint 57 SQLCMD Rollback Script for Prime/Gator/NRC10
+
+\Sprint_docs\Sprint57_docs\S57_PrimeGatorNRC10 - Rollback.sql
+
+*/
+
+:r "\ATLASRelease57&58\Sprint57_docs\S57 ATL-194 Sampling Householding - Rollback.sql"
---
+++ Sprint_docs/Sprint57_docs/S57_PrimeGatorNRC10.sql
@@ -0,0 +1,9 @@
+/*
+
+Sprint 57 SQLCMD Script for Prime/Gator/NRC10
+
+\Sprint_docs\Sprint57_docs\S57_PrimeGatorNRC10.sql
+
+*/
+
+:r "\ATLASRelease57&58\Sprint57_docs\S57 ATL-194 Sampling Householding.sql"
