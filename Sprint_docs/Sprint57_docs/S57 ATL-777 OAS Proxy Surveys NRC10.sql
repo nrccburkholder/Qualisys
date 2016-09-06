face155bdbe31c3d93b6fb890bc2deef7883e53f
tbutler <tbutler@nationalresearch.com>
2016-09-06 08:04:58 -0500

    New: S57 ATL-777 OAS Proxy Surveys -- modified etl_ProcessSamplePopulationDispositionLogRecords -- added Proxy disposition to SurveyTypeDispositions

---
+++ Sprint_docs/Sprint57_docs/S57 ATL-777 OAS Proxy Surveys NRC10.sql
@@ -0,0 +1,40 @@
+/*
+
+	S57 ATL-744 OAS Proxy Surveys
+	As an approved OAS CAHPS vendor, we need to exclude returns completed by proxies from submission, so that we comply with CAHPS requirements.
+
+	ATL-777 Add logic to the catalyst etl OAS post-processing procedure
+
+
+	Returned mail survey w/ Q37 = "answered for me" has proxy dispo in disposition logs.
+	Response to Q36 (did someone help) has no impact.
+	Neither complete nor partial dispositions should be final dispo.
+	If no other dispos, final should be "refusal"
+	If other dispos (e.g. incapacitated), the highest ranking should be final.
+
+	Tim Butler
+
+
+	insert into SurveyTypeDispositions
+
+*/
+
+
+USE QP_PROD
+GO
+
+declare @surveytype_id int 
+
+SELECT @surveytype_id = SurveyType_id
+FROM dbo.SurveyType
+WHERE SurveyType_dsc = 'OAS CAHPS'
+
+declare @proxyId int
+select @proxyId = disposition_id from disposition where strdispositionlabel = 'Proxy Return'
+
+
+if not exists(select 1 from SurveyTypeDispositions where SurveyType_ID = @surveytype_id and Disposition_ID = @proxyId)
+insert into SurveyTypeDispositions (Value, [Desc], Hierarchy, Disposition_ID, ExportReportResponses, SurveyType_ID)
+values ('320','Survey completed by Proxy',0,@proxyId,1,@surveytype_id)
+
+GO
