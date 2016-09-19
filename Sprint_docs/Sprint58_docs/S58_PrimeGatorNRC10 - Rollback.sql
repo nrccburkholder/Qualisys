7f6d11653c2a4d1a1f572d9c62ace4e2689be3af
tbutler <tbutler@nationalresearch.com>
2016-09-19 15:25:14 -0500

    S58_PrimeGatorNRC10 - added scripts for ATL-772

--- Sprint_docs/Sprint58_docs/S58_PrimeGatorNRC10 - Rollback.sql
+++ Sprint_docs/Sprint58_docs/S58_PrimeGatorNRC10 - Rollback.sql
@@ -7,3 +7,8 @@ Sprint 58 SQLCMD Script for Prime/Gator/NRC10
 */
 
 :r "\ATLASRelease57&58\Sprint58_docs\S58 ATL-718 Remove Systematic Sampling target record - rollback.sql"
+
+
+:r "\ATLASRelease57&58\Sprint58_docs\S55 ATL-683 DRG Update Rollback Process ROLLBACK.sql"
+
+:r "\ATLASRelease57&58\Sprint58_docs\S58 ATL-772 DRG Update Rollback UI NRC10 - ROLLBACK.sql"
