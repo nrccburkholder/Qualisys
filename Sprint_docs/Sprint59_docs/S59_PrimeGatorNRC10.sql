ee8449cfc7c57a70f06cd1cab56b8b7bd2e0dd35
tbutler <tbutler@nationalresearch.com>
2016-10-04 15:57:45 -0500

    Added S59_PrimeGatorNRC10 and S59_catsql01-catdb2 scripts

---
+++ Sprint_docs/Sprint59_docs/S59_PrimeGatorNRC10.sql
@@ -0,0 +1,16 @@
+/*
+
+Sprint 59 SQLCMD Script for Prime/Gator/NRC10
+
+\Sprint_docs\Sprint59_docs\S59_PrimeGatorNRC10.sql
+
+*/
+
+:r "\ATLASRelease59&60\Sprint59_docs\S59 ATL-628 Fix Letter-Web Mailing Step for Comments importing.sql"
+
+:r "\ATLASRelease59&60\Sprint59_docs\S59 ATL-862 UPDATE ACO_PQRS Completeness.sql"
+
+:r "\ATLASRelease59&60\Sprint59_docs\S59 ATL-864 Update ACO & PQRS CEM Templates.sql"
+
+:r "\ATLASRelease59&60\Sprint59_docs\S59 ATL-871 Processing for ACO_PQRS Bad Addr & Bad Phone NRC10.sql"
+
---
+++ Sprint_docs/Sprint59_docs/S59_catsql01-catdb2.sql
@@ -0,0 +1,9 @@
+/*
+
+Sprint 59 SQLCMD Script for catsql01-catdb2
+
+\Sprint_docs\Sprint59_docs\S59_catsql01-catdb2.sql
+
+*/
+
+:r "\ATLASRelease59&60\Sprint59_docs\S59 ATL-871 Processing for ACO_PQRS Bad Addr & Bad Phone CATDB.sql"
