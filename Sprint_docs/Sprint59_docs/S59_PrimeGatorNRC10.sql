323d4ff46bc531a5d1b227966b09f1267ee995b2
tbutler <tbutler@nationalresearch.com>
2016-10-04 15:59:04 -0500

    Added bug fix script to S59_PrimeGatorNRC10.sql

--- Sprint_docs/Sprint59_docs/S59_PrimeGatorNRC10.sql
+++ Sprint_docs/Sprint59_docs/S59_PrimeGatorNRC10.sql
@@ -14,3 +14,5 @@ Sprint 59 SQLCMD Script for Prime/Gator/NRC10
 
 :r "\ATLASRelease59&60\Sprint59_docs\S59 ATL-871 Processing for ACO_PQRS Bad Addr & Bad Phone NRC10.sql"
 
+:r "\ATLASRelease59&60\Sprint59_docs\S59 ATL-957 INC0061708 - fix bug in SV_CAHPS_SamplePeriods.sql"
+
