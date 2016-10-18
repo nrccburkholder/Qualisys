f089a5e7236f451f62ae2f0c8e11f67d9fa93daf
Chris Burkholder <cburkholder@nationalresearch.com>
2016-10-18 15:53:41 -0500

    Deployment scripts + added GO for deployment

---
+++ Sprint_docs/Sprint60_docs/S60_PrimeGatorNRC10 - Rollback.sql
@@ -0,0 +1,14 @@
+/*
+
+Sprint 60 SQLCMD Rollback Script for Prime/Gator/NRC10
+
+\Sprint_docs\Sprint60_docs\S60_PrimeGatorNRC10 - Rollback.sql
+
+*/
+
+--:r "\ATLASRelease59&60\Sprint60_docs\S60 ATL-987 SV_CAHPS_SamplePeriods Disable for CIHI - Rollback.sql"
+
+:r "\ATLASRelease59&60\Sprint60_docs\S60 ATL-974 DRG Update-Rollback modification -- ROLLBACK.sql"
+
+:r "\ATLASRelease59&60\Sprint60_docs\S60 ATL-972 Add a AllowAutoSample bit field to ClientGroup - ROLLBACK.sql"
+
---
+++ Sprint_docs/Sprint60_docs/S60_PrimeGatorNRC10.sql
@@ -0,0 +1,14 @@
+/*
+
+Sprint 60 SQLCMD Script for Prime/Gator/NRC10
+
+\Sprint_docs\Sprint60_docs\S60_PrimeGatorNRC10.sql
+
+*/
+
+:r "\ATLASRelease59&60\Sprint60_docs\S60 ATL-987 SV_CAHPS_SamplePeriods Disable for CIHI.sql"
+
+:r "\ATLASRelease59&60\Sprint60_docs\S60 ATL-974 DRG Update-Rollback modification.sql"
+
+:r "\ATLASRelease59&60\Sprint60_docs\S60 ATL-972 Add a AllowAutoSample bit field to ClientGroup.sql"
+
