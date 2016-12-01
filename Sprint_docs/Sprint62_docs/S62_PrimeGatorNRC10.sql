35172c1a128e676477ffcc10783cbca55bedaddd
Chris Burkholder <cburkholder@nationalresearch.com>
2016-12-01 13:41:50 -0600

    Restored back populate script to deploy

--- Sprint_docs/Sprint62_docs/S62_PrimeGatorNRC10.sql
+++ Sprint_docs/Sprint62_docs/S62_PrimeGatorNRC10.sql
@@ -8,6 +8,6 @@ Sprint 62 SQLCMD Script for Prime/Gator/NRC10
 
 :r "\ATLASRelease61&62\Sprint62_docs\S62 ATL-1103 Disposition TOCLs for ACO & PQRS NRC10.sql"
 
---:r "\ATLASRelease61&62\Sprint62_docs\S62 ATL-1103 Disposition TOCLs for ACO & PQRS - BACKPOPULATE - NRC10.sql"
+:r "\ATLASRelease61&62\Sprint62_docs\S62 ATL-1103 Disposition TOCLs for ACO & PQRS - BACKPOPULATE - NRC10.sql"
 
 :r "\ATLASRelease61&62\Sprint62_docs\S62 ATL-1002 Eliminate Hospice CAHPS Guardian Flag DQ.sql"
