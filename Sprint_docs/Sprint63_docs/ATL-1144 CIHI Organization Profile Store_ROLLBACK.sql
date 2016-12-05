5641f18c7f1ab7e4eacb24aa95468b54498cac0e
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2016-12-04 18:20:28 -0600

    ATL-1144 added the 3 QA tables to the rollback script

--- Sprint_docs/Sprint63_docs/ATL-1144 CIHI Organization Profile Store_ROLLBACK.sql
+++ Sprint_docs/Sprint63_docs/ATL-1144 CIHI Organization Profile Store_ROLLBACK.sql
@@ -1,8 +1,5 @@
 use qp_prod
 
-if exists (select * from sys.schemas where name = 'CIHI')
-	exec ('DROP SCHEMA CIHI');
-
 IF EXISTS (SELECT * FROM SYS.TABLES where schema_name(schema_id)='CIHI' and name = 'Submission')
 	DROP TABLE [CIHI].[Submission];
 
@@ -15,4 +12,14 @@ if exists (select * from sys.objects where schema_name(schema_id)='CIHI' and nam
 IF EXISTS (SELECT * FROM SYS.TABLES where schema_name(schema_id)='CIHI' and name = 'OrgProfile')
 	DROP TABLE [CIHI].[OrgProfile];
 
+IF EXISTS (SELECT * FROM SYS.TABLES where schema_name(schema_id)='CIHI' and name = 'QA_QuestionnaireCycleAndStrata')
+	DROP TABLE [CIHI].[QA_QuestionnaireCycleAndStrata];
 
+IF EXISTS (SELECT * FROM SYS.TABLES where schema_name(schema_id)='CIHI' and name = 'QA_Questionnaire')
+	DROP TABLE [CIHI].[QA_Questionnaire];
+
+IF EXISTS (SELECT * FROM SYS.TABLES where schema_name(schema_id)='CIHI' and name = 'QA_Question')
+	DROP TABLE [CIHI].[QA_Question];
+
+if exists (select * from sys.schemas where name = 'CIHI')
+	exec ('DROP SCHEMA CIHI');
