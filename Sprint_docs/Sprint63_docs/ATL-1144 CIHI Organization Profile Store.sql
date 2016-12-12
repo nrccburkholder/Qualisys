c4c148e0040ef579cd5ab864e55be8c85b19c5f9
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2016-12-12 15:15:52 -0600

    added CPESManualVersionCD cuz it's different than CPESVersionCD

--- Sprint_docs/Sprint63_docs/ATL-1144 CIHI Organization Profile Store.sql
+++ Sprint_docs/Sprint63_docs/ATL-1144 CIHI Organization Profile Store.sql
@@ -11,6 +11,7 @@ CREATE TABLE [CIHI].[Submission] (
 	DeviceManufacturer varchar(15) DEFAULT 'N0002',
 	OrganizationCD varchar(20),
 	CPESVersionCD varchar(20) DEFAULT 'CPES-IC_1.0',
+	CPESManualVersionCD varchar(20) DEFAULT 'CPES-IC_PM_V1.0',
 	SubmissionSubject char(3),
 	SubmissionTypeCD char(2),
 	PurposeCD char(1),
