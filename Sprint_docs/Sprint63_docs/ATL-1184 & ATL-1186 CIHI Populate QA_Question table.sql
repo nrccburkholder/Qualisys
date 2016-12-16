8089975fdfcd516e308a203e2d3e3d1db7891f15
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2016-12-16 09:41:31 -0600

    UNION SELECT needed another field

--- Sprint_docs/Sprint63_docs/ATL-1184 & ATL-1186 CIHI Populate QA_Question table.sql
+++ Sprint_docs/Sprint63_docs/ATL-1184 & ATL-1186 CIHI Populate QA_Question table.sql
@@ -110,10 +110,11 @@ BEGIN
 	join SkipQstns sq on si.skip_id = sq.skip_id
 	join cihi.recode r on convert(varchar,surv.qstncore)=r.nrcvalue and r.FinalField='questionnaireCycle.questionnaire.questions.question.code_code'
 	-- unanswered gate questions invoke their skips 
-	union select distinct surv.qstnCore, surv.intResponseVal, sq.QstnCore, surv.samplepopID
+	union select distinct surv.qstnCore, surv.intResponseVal, sq.QstnCore, surv.samplepopID, r.cihiValue
 	from #surveyInfo surv
 	join SkipIdentifier si on si.survey_id = surv.survey_id and si.qstnCore = surv.qstnCore and surv.intResponseVal<0 and si.datGenerated = surv.datGenerated
 	join SkipQstns sq on si.skip_id = sq.skip_id
+	join cihi.recode r on convert(varchar,surv.qstncore)=r.nrcvalue and r.FinalField='questionnaireCycle.questionnaire.questions.question.code_code'
 
 	declare @currentGate varchar(10)
 	select @currentGate = min(gateQstn) from #cihiInvokedGate
