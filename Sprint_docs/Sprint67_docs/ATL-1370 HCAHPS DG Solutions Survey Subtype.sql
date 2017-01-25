5e25804ac2e025eb846c9989f905e22feb7a8544
Chris Burkholder <cburkholder@nationalresearch.com>
2017-01-25 17:08:38 -0600

    ATL-1370 initial commit: Template definitions and MakeTemplateFromSurveys

---
+++ Sprint_docs/Sprint67_docs/ATL-1370 HCAHPS DG Solutions Survey Subtype.sql
@@ -0,0 +1,32 @@
+/*
+ATL-1370 HCAHPS DG Solutions Survey Subtype.sql
+
+Chris Burkholder
+
+1/24/2017
+
+INSERT INTO Subtype
+INSERT INTO SurveyTypeSubtype
+
+select * from surveysubtype
+select * from subtype
+select * from surveytypesubtype
+select * from subtypecategory
+*/
+USE [QP_Prod]
+GO
+
+--update subtype set subtype_nm = 'RT' where subtype_nm = 'DG'
+
+if not exists (select * from subtype where subtype_nm = 'RT')
+insert into subtype (Subtype_nm,SubtypeCategory_id,bitRuleOverride,bitQuestionnaireRequired,bitActive)
+values ('RT',1,0,0,1)
+
+declare @DG int 
+select @DG = Subtype_id from subtype where subtype_nm = 'RT'
+
+if not exists (select * from surveytypesubtype where surveytype_id = 2)
+insert into SurveyTypeSubType(SurveyType_id,Subtype_id)
+values (2, @DG)
+
+GO
