a180113d15ebd1ff9b3e72a7d67e03c55f50ef57
Chris Burkholder <cburkholder@nationalresearch.com>
2017-03-07 10:18:33 -0600

    Update to call period creation out a level

--- Sprint_docs/Sprint70_docs/S70 RTP-1637 create sample periods prior to sampling 1st sampleset of the quarter - Rollback.sql
+++ Sprint_docs/Sprint70_docs/S70 RTP-1637 create sample periods prior to sampling 1st sampleset of the quarter - Rollback.sql
@@ -5,8 +5,8 @@
 
 	3/6/2017
 
-	ALTER PROCEDURE [dbo].[QCL_SelectActivePeriodbySurveyId]
 	CREATE PROCEDURE [dbo].[QCL_InsertQuarterlyRTPeriodsbySurveyId]
+	ALTER PROCEDURE [dbo].[QCL_SelectSamplePeriodsBySurvey]
 
 PeriodDef_id	Survey_id	Employee_id	datAdded	strPeriodDef_nm	intExpectedSamples	DaysToSample	datExpectedEncStart	datExpectedEncEnd	strDayOrder	MonthWeek	SamplingMethod_id
 457135	20617	954	2017-01-12 16:17:12.320	Jan17	31	2	2017-01-01 00:00:00.000	2017-01-31 00:00:00.000	NULL	D	1
@@ -52,85 +52,40 @@ Modified:
 
 */
 
-ALTER PROCEDURE [dbo].[QCL_SelectActivePeriodbySurveyId]
+ALTER   PROCEDURE [dbo].[QCL_SelectSamplePeriodsBySurvey]
 	@survey_id int
 AS
+
 SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED  
+CREATE TABLE #activePeriod (periodDef_id int, ActivePeriod bit default 0)
 
-CREATE TABLE #periods (periodDef_id int, ActivePeriod bit default 0)
+INSERT INTO #activePeriod
+EXEC [dbo].[QCL_SelectActivePeriodbySurveyId] @survey_id
 
---Get a list of all periods for this survey
-INSERT INTO #periods (periodDef_id)
-SELECT periodDef_id
-FROM perioddef
-WHERE survey_id=@survey_id
+SELECT p.PeriodDef_id, Survey_id, Employee_id, datAdded, strPeriodDef_nm,
+    intExpectedSamples, datExpectedEncStart, datExpectedEncEnd,
+    SamplingMethod_id, DaysToSample, monthWeek, coalesce(a.ActivePeriod,0) as ActivePeriod,
+	case
+		when a.ActivePeriod is not null then 1
+		else 0
+	end as TimeFrame
+INTO #AllPeriods
+FROM PeriodDef p LEFT JOIN #activePeriod a
+ON p.perioddef_id=a.perioddef_id 
+WHERE p.survey_id =@survey_id
 
---Get a list of all periods that have not completed sampling
-SELECT distinct pd.PeriodDef_id
-INTO #temp
-FROM perioddef p, perioddates pd
-WHERE p.perioddef_id=pd.perioddef_id AND
-		survey_id=@survey_id AND
-	  	datsampleCREATE_dt is null
-
---Find the active Period.  It is either a period that hasn't completed sampling
---or a period that hasn't started but has the most recent first scheduled date 
---If no unfinished periods exist, set active period to the period with the most
---recently completed sample 
-
-IF EXISTS (SELECT top 1 *
-			FROM #temp)
-BEGIN
-	
-	DECLARE @UnfinishedPeriod int
-	
-	SELECT @UnfinishedPeriod=pd.perioddef_id
-	FROM perioddates pd, #temp t
-	WHERE pd.perioddef_id=t.perioddef_id AND
-		  	pd.samplenumber=1 AND
-			pd.datsampleCREATE_dt is not null
-	
-	IF @UnfinishedPeriod is not null
-	BEGIN
-		--There is a period that is partially finished, so set it to be active
-		UPDATE #periods
-		SET ActivePeriod=1
-		WHERE perioddef_id = @UnfinishedPeriod
-	END
-	ELSE
-	BEGIN
-		--There is no period that is partially finished, so set the unstarted period
-		--with the earliest scheduled sample date to be active
-		UPDATE #periods
-		SET ActivePeriod=1
-		WHERE perioddef_id =
-			(SELECT top 1 pd.perioddef_id
-			 FROM perioddates pd, #temp t
-			 WHERE pd.perioddef_id=t.perioddef_id AND
-				  	pd.samplenumber=1
-			 ORDER BY datscheduledsample_dt)
-	END
-END
-ELSE
-BEGIN
-	--No unfinished periods exist, so we will set the active to be the most recently
-	--finished
-	UPDATE #periods
-	SET ActivePeriod=1
-	WHERE perioddef_id =
-		(SELECT top 1 p.perioddef_id
-		 FROM perioddates pd, perioddef p
-		 WHERE p.survey_id=@survey_id AND
-				pd.perioddef_id=p.perioddef_id
-		 GROUP BY p.perioddef_id
-		 ORDER BY Max(datsampleCREATE_dt) desc)
-END
+--Mark any periods that are in the future
+UPDATE #AllPeriods
+SET TimeFrame=2
+WHERE perioddef_id in 
+	(select p.perioddef_id
+	 from #AllPeriods p, perioddates pd
+	 WHERE p.perioddef_id=pd.perioddef_id and
+			p.activeperiod=0 and
+			pd.samplenumber=1 and
+			pd.datsamplecreate_dt is null)
 
 SELECT *
-FROM #periods
-WHERE ActivePeriod=1
-
-DROP TABLE #temp
-DROP TABLE #periods
+FROM #AllPeriods
 
 GO
