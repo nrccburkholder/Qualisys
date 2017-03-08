ac9a30c8bba0cb70360cedbf12826c9b2c3e0631
Chris Burkholder <cburkholder@nationalresearch.com>
2017-03-08 10:42:33 -0600

    Deployment Files S70 + Housekeeping S67,S68

---
+++ Sprint_docs/Sprint70_docs/S70_PrimeGatorNRC10.sql
@@ -0,0 +1,17 @@
+/*
+
+Sprint 70 SQLCMD Script for Prime/Gator/NRC10
+
+\Sprint_docs\Sprint70_docs\S70_PrimeGatorNRC10.sql
+
+*/
+
+:r "\ATLASRelease69&70\Sprint70_docs\S70 ATL-1400 HCAHPS Don't Household Homeless.sql"
+
+:r "\ATLASRelease69&70\Sprint70_docs\S70 RTP-1637 create sample periods prior to sampling 1st sampleset of the quarter.sql"
+
+:r "\ATLASRelease69&70\Sprint70_docs\S70 RTP-1639 HCAHPS+RT Oversamples.sql"
+
+:r "\ATLASRelease69&70\Sprint70_docs\S70 RTP-1672 Prioritize Qualisys-HCAHPS above RT-HCAHPS.sql"
+
+:r "\ATLASRelease69&70\Sprint70_docs\S70 RTP-1677 Create OutreachRequest tables.sql"
