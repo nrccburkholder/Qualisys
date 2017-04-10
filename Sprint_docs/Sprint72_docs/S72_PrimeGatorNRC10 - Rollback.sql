ac9a033c18bd4997222dcfecd8018031951d8c75
Chris Burkholder <cburkholder@nationalresearch.com>
2017-04-10 17:14:12 -0500

    Reordered rollback for _removed tables dependency

--- Sprint_docs/Sprint72_docs/S72_PrimeGatorNRC10 - Rollback.sql
+++ Sprint_docs/Sprint72_docs/S72_PrimeGatorNRC10 - Rollback.sql
@@ -8,6 +8,6 @@ Sprint 72 SQLCMD Rollback Script for Prime/Gator/NRC10
 
 :r "\ATLASRelease71&72\Sprint72_docs\S72_ATL-1443 Update Study Data Structure Process [QP_Prod] - Rollback.sql"
 
-:r "\ATLASRelease71&72\Sprint72_docs\S72_ATL-1079 Modify default DQ rule - Rollback.sql"
-
 :r "\ATLASRelease71&72\Sprint72_docs\S72_ATL-1080 Backpopulate existing OAS surveys DQ rules - Rollback.sql"
+
+:r "\ATLASRelease71&72\Sprint72_docs\S72_ATL-1079 Modify default DQ rule - Rollback.sql"
