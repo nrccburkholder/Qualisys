6902d27e05ba842f754b8883e0960bd41d741df6
Chris Burkholder <cburkholder@nationalresearch.com>
2017-04-11 14:23:54 -0500

    Updated deploy scripts

--- Sprint_docs/Sprint72_docs/S72_WonderwomanCycloneMars.sql
+++ Sprint_docs/Sprint72_docs/S72_WonderwomanCycloneMars.sql
@@ -11,3 +11,5 @@ Sprint 72 SQLCMD Script for Wonderwoman/Cyclone/Mars
 :r "\ATLASRelease71&72\Sprint72_docs\S72 ATL-1430 Update DTS Packages to Use OASEligibleSurg.sql"
 
 :r "\ATLASRelease71&72\Sprint72_docs\S72_ATL-1429 Update OAS CPT Code Exclusions [QP_Load].sql"
+
+:r "\ATLASRelease71&72\Sprint72_docs\S72_ATL-1078 Update the QLoader CPT codes function.sql"
