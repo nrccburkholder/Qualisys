52dcbbc7850028ee73d6044d6fcd011db80fce4d
Chris Burkholder <cburkholder@nationalresearch.com>
2017-04-26 17:58:41 -0500

    Update for one more email address for Ted's branding change

--- Sprint_docs/Sprint72_docs/S72_NRCAuthAdmin Branding Email Update - Rollback.sql
+++ Sprint_docs/Sprint72_docs/S72_NRCAuthAdmin Branding Email Update - Rollback.sql
@@ -5,7 +5,7 @@
 
 	4/14/2017
 
-	Update QualPro_Params WWWNRCPickerUrl
+	Update QualPro_Params WWWNRCPickerUrl,'NAAMailFromAccount','NAAMassEmailFrom'
 
 */
 
@@ -20,6 +20,6 @@ from qualpro_params where strparam_nm = 'WWWNRCPickerUrl'
 update qualpro_params set strparam_value =
 --select strparam_value,
 replace(strparam_value, 'NRCHealth', 'NationalResearch')
-from qualpro_params where strparam_nm = 'NAAMailFromAccount'
+from qualpro_params where strparam_nm in ('NAAMailFromAccount','NAAMassEmailFrom')
 
 GO
