0ed0cf7688e888f81776ca876901a870a68688f8
Dave Gilsdorf <dgilsdorf@nationalresearch.com>
2017-05-02 14:56:25 -0500

    RTP-2431 Add Export_HCAHPSSampleunitAvailableCount proc

--- Sprint_docs/SprintRTPQ2.1_docs/RTP-2431 DRG Updated Non-Sampled Records.sql
+++ Sprint_docs/SprintRTPQ2.1_docs/RTP-2431 DRG Updated Non-Sampled Records.sql
@@ -14,6 +14,36 @@ GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+/*
+Business Purpose:
+This procedure is used to calculate the number of eligible discharges.  It
+is used IN the header record of the CMS export
+
+Created:	06/22/2006 by DC
+
+Modified:
+			4/15/2010 MWB: changed logic to count distinct pop_ID only instead of pop_ID,enc_ID
+			(which esentially counted distinct Enc_IDs)
+			1/14/2015 CJB: switched from HCAHPS specific table to new EligibleEncLog table
+
+
+*/
+
+ALTER PROCEDURE [dbo].[Export_HCAHPSSampleunitAvailableCount]
+ @Sampleunit_id INT,
+    @startDate DATETIME,
+    @EndDate DATETIME
+AS
+
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+select count(*)
+from (select distinct pop_id
+  from EligibleEncLog
+  where sampleunit_id=@sampleunit_id
+   and SampleEncounterDate between @startDate and dateadd(s,-1,dateadd(d,1,@EndDate))) p
+GO
 ALTER PROCEDURE [dbo].[LD_UpdateDRG_UpdateRollback] @Study_ID int, @DataFile_id int, @DRGOption varchar(20) = 'DRG'
 AS
 
