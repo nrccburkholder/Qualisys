05d068e6eb0ca2db285a415c00b2d4506e28c836
Chris Burkholder <cburkholder@nationalresearch.com>
2017-05-24 16:45:49 -0500

    Retry on H HH ICH fix

--- Legacy/Delphi Shared Code/FP/uLayoutCalc.pas
+++ Legacy/Delphi Shared Code/FP/uLayoutCalc.pas
@@ -1260,7 +1260,7 @@ begin
     _ParenClose := ')';
   end;
 
-  if CAHPSNumbering then begin
+  if DoDBenSkips or CAHPSNumbering then begin
     _PCLBoldOn := PCLBoldOn;
     _PCLBoldOff := PCLBoldOff;
   end
