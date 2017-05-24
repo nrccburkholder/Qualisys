c36b03a9461b9d7889ad50d394a49aa6fef38c09
Chris Burkholder <cburkholder@nationalresearch.com>
2017-05-24 15:52:28 -0500

    Added Form Layout version bump script

---
+++ Sprint_docs/SprintRtp2017q2s3_docs/ATL-1450 skip instructions are no longer Bolded.sql
@@ -0,0 +1,15 @@
+/*
+	ATL-1450 skip instructions are no longer Bolded.sql
+
+	Chris Burkholder
+
+	5/24/2017
+
+	Update QualPro_params (Form Layout version only)
+
+	select * from qualpro_params where strparam_nm like 'Formlayout%'
+
+*/
+
+update QualPro_Params set strparam_value = '3.39'
+where strparam_nm = 'FormLayoutVersion'
