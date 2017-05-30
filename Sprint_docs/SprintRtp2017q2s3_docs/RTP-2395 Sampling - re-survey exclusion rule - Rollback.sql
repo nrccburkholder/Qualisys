6aa6feac333042d6eaed2a703ce701e8d665533f
Chris Burkholder <cburkholder@nationalresearch.com>
2017-05-30 16:43:15 -0500

    Added new resurvey field to DB

---
+++ Sprint_docs/SprintRtp2017q2s3_docs/RTP-2395 Sampling - re-survey exclusion rule - Rollback.sql
@@ -0,0 +1,24 @@
+/*
+	    RTP-2395 Sampling - re-survey exclusion rule - Rollback.sql
+
+		Chris Burkholder
+
+		5/30/2017
+
+		ADD COLUMN to SURVEY_DEF, Survey_DefTemplate
+
+		select t.name,c.name,* from sys.columns c inner join sys.tables t on c.object_id = t.object_id where c.name like '%resurvey%'
+*/
+
+use qp_prod
+go
+
+if exists(select * from sys.columns where name = 'LocationFacilityResurveyDays'
+			and object_id = object_id('dbo.survey_def'))
+	alter table dbo.survey_def drop column LocationFacilityResurveyDays
+
+if exists(select * from sys.columns where name = 'LocationFacilityResurveyDays'
+			and object_id = object_id('rtphoenix.survey_defTemplate'))
+	alter table rtphoenix.survey_defTemplate drop column LocationFacilityResurveyDays
+
+go
