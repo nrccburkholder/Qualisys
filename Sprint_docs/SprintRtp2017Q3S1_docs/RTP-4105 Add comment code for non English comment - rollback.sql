b6f7227e2efa0ec2f986e8584aa69f136e958b08
Lanny Boswell <lboswell@nationalresearch.com>
2017-08-08 13:10:12 -0500

    Add a comment code for "Non-English Original Comment"

---
+++ Sprint_docs/SprintRtp2017Q3S1_docs/RTP-4105 Add comment code for non English comment - rollback.sql
@@ -0,0 +1,21 @@
+/*
+	RTP-4105 Add comment code for non English comment - rollback.sql
+
+	Lanny Boswell
+
+	Delete CommentCodes
+
+*/
+
+use [QP_Prod]
+go
+
+IF EXISTS (SELECT * FROM CommentCodes WHERE CmntCode_id=1016 AND strCmntCode_Nm='Non-English Original Comment')
+BEGIN
+	DELETE CommentCodes WHERE CmntCode_id=1016 AND strCmntCode_Nm='Non-English Original Comment'
+
+	UPDATE dbo.CommentCodes 
+	SET intOrder=intOrder-1
+	WHERE CmntSubHeader_id=1
+	AND intOrder>19
+END
---
+++ Sprint_docs/SprintRtp2017Q3S1_docs/RTP-4105 Add comment code for non English comment.sql
@@ -0,0 +1,28 @@
+/*
+	RTP-4105 Add comment code for non English comment.sql
+
+	Lanny Boswell
+
+	Insert CommentCodes
+
+*/
+
+use [QP_Prod]
+go
+
+IF EXISTS (SELECT * FROM CommentCodes WHERE CmntCode_id=1016 AND strCmntCode_Nm<>'Non-English Original Comment')
+BEGIN
+       RAISERROR(N'Another code already uses CmntCode_id=1016',10,1)
+END
+ELSE IF NOT EXISTS (SELECT * FROM CommentCodes WHERE CmntCode_id=1016 AND strCmntCode_Nm='Non-English Original Comment')
+BEGIN
+       UPDATE dbo.CommentCodes 
+       SET intOrder=intOrder+1
+       WHERE CmntSubHeader_id=1
+       AND intOrder>=19
+
+       SET IDENTITY_INSERT dbo.CommentCodes ON
+       INSERT INTO dbo.CommentCodes (CmntCode_id, CmntSubHeader_id, strCmntCode_Nm, bitRetired, strModifiedBy, datModified, intOrder, Disposition_id)
+       VALUES (1016, 1, 'Non-English Original Comment', 0, 'dgilsdorf', getdate(), 19, NULL)
+       SET IDENTITY_INSERT dbo.CommentCodes Off
+END
