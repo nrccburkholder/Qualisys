e4d8f1cb8b7b2e9ddbc5f9e3ef1463a05168b49a
Lanny Boswell <lboswell@nationalresearch.com>
2017-08-30 10:39:17 -0500

    refactored the query

--- Sprint_docs/SprintRtp2017Q3S2_docs/RTP-3949 Create CalculateOasHhResponseRate.sql
+++ Sprint_docs/SprintRtp2017Q3S2_docs/RTP-3949 Create CalculateOasHhResponseRate.sql
@@ -25,24 +25,9 @@ SET NOCOUNT ON
 declare @EncDateStart datetime, @EncDateEnd datetime
 exec QCL_CreateCAHPSRollingYear @PeriodDate, @SurveyType_id, @EncDateStart OUTPUT, @EncDateEnd OUTPUT;
 
-;with SamplePops as
+;with PossibleSamplePops as
 (
-	select case when 
-			( exists (select * from DispositionLog dl
-				where dl.Disposition_id in (19, 20)
-				and dl.SamplePop_id = sp.SAMPLEPOP_ID ) 
-			or exists (select * from QUESTIONFORM qf
-				where qf.bitComplete = 1
-				and qf.SAMPLEPOP_ID = sp.SAMPLEPOP_ID ) )
-			and not exists (select * from DispositionLog dl
-				where dl.Disposition_id in (3, 4, 8, 10)
-				and dl.SamplePop_id = sp.SAMPLEPOP_ID ) 
-		then 1 else 0 end as IsCompleted,
-		case when 
-			not exists (select * from DispositionLog dl
-				where dl.Disposition_id in (3, 4, 8, 10)
-				and dl.SamplePop_id = sp.SAMPLEPOP_ID ) 
-		then 1 else 0 end as IsEligible
+	select distinct sp.SAMPLEPOP_ID
 	from SUFacility sf
 	inner join SAMPLEUNIT su
 		on su.SUFacility_ID = sf.SuFacility_ID
@@ -50,15 +35,42 @@ exec QCL_CreateCAHPSRollingYear @PeriodDate, @SurveyType_id, @EncDateStart OUTPU
 		ON su.SAMPLEUNIT_ID = ssut.SAMPLEUNIT_ID
 	inner join SAMPLESET sset
 		ON sset.SAMPLESET_ID = ssut.SAMPLESET_ID 
-		and sset.datDateRange_FromDate >= @EncDateStart 
-		and sset.datDateRange_ToDate <= @EncDateEnd
 	inner join SAMPLEPOP sp
 		on sp.SAMPLESET_ID = sset.SAMPLESET_ID
 	where sf.MedicareNumber = @MedicareNumber
 		and su.CAHPSType_id = @SurveyType_id
+		and sset.datDateRange_FromDate >= @EncDateStart 
+		and sset.datDateRange_ToDate <= @EncDateEnd
+),
+EligibleSamplePops as
+(
+	select distinct sp.SAMPLEPOP_ID
+	from PossibleSamplePops sp
+	left join DispositionLog dl
+		on dl.SamplePop_id = sp.SAMPLEPOP_ID
+		and dl.Disposition_id in (3, 4, 8, 10)
+	where dl.SamplePop_id is null
+),
+QfSamplePops as
+(
+	select sp.SAMPLEPOP_ID, isnull(max(cast(qf.bitComplete as int)), 0) as IsQfCompleted
+	from EligibleSamplePops sp
+	left join QUESTIONFORM qf
+		on qf.SAMPLEPOP_ID = sp.SAMPLEPOP_ID
 	group by sp.SAMPLEPOP_ID
+),
+SamplePops as
+(
+	select distinct sp.SAMPLEPOP_ID, 
+		case when sp.IsQfCompleted = 1 
+			or dl.SamplePop_id is not null then 1 else 0 end as IsCompleted
+	from QfSamplePops sp
+	left join DispositionLog dl
+		on dl.SamplePop_id = sp.SAMPLEPOP_ID
+		and dl.Disposition_id in (19, 20)
 )
-select 100.0 * isnull(sum(IsCompleted), 0) / isnull(sum(IsEligible), 0) as RespRate
+select 100.0 * isnull(sum(IsCompleted), 0) / 
+	case when count(*) = 0 then 1 else count(*) end as RespRate
 from SamplePops
 
 SET TRANSACTION ISOLATION LEVEL READ COMMITTED
