c2111b86c2d04807310971d7eb6b17f99e3c97d4
dgilsdorf <dgilsdorf@nrchealth.com>
2017-09-26 13:14:25 -0500

    RTP-4840 HCAHPS Export Double-Counting Ineligibles - initial commit deployed with an Emergency CR

---
+++ Sprint_docs/SprintRtp2017Q3S4_docs/RTP-4840 deployed with an Emergency CR - HCAHPS Export Double-Counting Ineligibles - rollback.sql
@@ -0,0 +1,41 @@
+/*
+	RTP-4840 HCAHPS Export Double-Counting Ineligibles
+	
+	Deployed with an Emergency Change Request - not to be included in a regular release
+
+	Dave Gilsdorf
+
+*/
+use qp_prod
+go
+/*
+Business Purpose:
+This procedure is used to calculate the number of eligible discharges.  It
+is used IN the header record of the CMS export
+
+Created:	06/22/2006 by DC
+
+Modified:
+			4/15/2010 MWB: changed logic to count distinct pop_ID only instead of pop_ID,enc_ID
+			(which esentially counted distinct Enc_IDs)
+			1/14/2015 CJB: switched from HCAHPS specific table to new EligibleEncLog table
+
+
+*/
+
+ALTER PROCEDURE [dbo].[Export_HCAHPSSampleunitAvailableCount]
+ @Sampleunit_id INT,
+    @startDate DATETIME,
+    @EndDate DATETIME
+AS
+
+SET NOCOUNT ON
+SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+
+select count(*)
+from (select distinct pop_id
+  from EligibleEncLog
+  where sampleunit_id=@sampleunit_id
+   and SampleEncounterDate between @startDate and dateadd(s,-1,dateadd(d,1,@EndDate))
+   and IneligibleAfterDRGUpdate=0) p
+GO
