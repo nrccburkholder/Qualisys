9c7c0991ee1998808e57298ab9c658e791a9459e
dgilsdorf <dgilsdorf@nrchealth.com>
2017-09-27 11:47:40 -0500

    RPT-4871 - conform Cantonese/Mandarin language codes

--- Sprint_docs/SprintRtp2017Q3S3_docs/RTP-4781 add Cantonese & Mandarin to Qualisys.sql
+++ Sprint_docs/SprintRtp2017Q3S3_docs/RTP-4781 add Cantonese & Mandarin to Qualisys.sql
@@ -7,8 +7,8 @@
 use qp_prod 
 go
 if not exists (select * from languages where langid=33 and language = 'Cantonese')
-	insert into languages (LangID, Language, Dictionary, WebLanguageLabel, QualiSysLanguage, ISO639) values (33, 'Cantonese', 'English.dct', 'Cantonese', 0, 'chi.can')
+	insert into languages (LangID, Language, Dictionary, WebLanguageLabel, QualiSysLanguage, ISO639) values (33, 'Cantonese', 'English.dct', 'Cantonese', 0, 'yue')
 if not exists (select * from languages where langid=34 and language = 'Mandarin')
-	insert into languages (LangID, Language, Dictionary, WebLanguageLabel, QualiSysLanguage, ISO639) values (34, 'Mandarin', 'English.dct', 'Mandarin', 0, 'chi.man')
+	insert into languages (LangID, Language, Dictionary, WebLanguageLabel, QualiSysLanguage, ISO639) values (34, 'Mandarin', 'English.dct', 'Mandarin', 0, 'cmn')
 
 GO
