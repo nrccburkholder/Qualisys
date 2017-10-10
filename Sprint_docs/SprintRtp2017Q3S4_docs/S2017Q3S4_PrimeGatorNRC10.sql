27644fee303712cc3e03100ca4cea1cfc95a8ca2
Chris Burkholder <cburkholder@nationalresearch.com>
2017-10-10 10:22:20 -0500

    Deploy scripts and zip for HCAHPSRT hot fix

---
+++ Sprint_docs/SprintRtp2017Q3S4_docs/S2017Q3S4_PrimeGatorNRC10.sql
@@ -0,0 +1,13 @@
+/*
+
+Sprint 2017 Q3 S4 SQLCMD Script for Prime/Gator/NRC10
+
+\Sprint_docs\SprintRtp2017q3s4_docs\S2017Q3S4_PrimeGatorNRC10.sql
+
+*/
+
+:r "\AtlasReleaseRT2017Q3HCAHPSRT\SprintRtp2017q3s4_docs\RTP-5056 MakeTemplateFromGoldenStudy logging and distinct fixes.sql" 
+
+:r "\AtlasReleaseRT2017Q3HCAHPSRT\SprintRtp2017q3s4_docs\RTP-5056 MakeSurveysFromTemplate TagFields PackageTable Date fixes.sql" 
+
+:r "\AtlasReleaseRT2017Q3HCAHPSRT\SprintRtp2017q3s4_docs\RTP-5056 MakeStudyFromTemplate TagFields PackageTable Date fixes.sql" 
